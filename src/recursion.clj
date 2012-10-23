d22db20b2b8ef5400e70bf89a80d669fba3346e8
Sami Saada <sami.saada@gmail.com>
2012-10-23 19:01:31 +0300

    seq= refactored

--- src/recursion.clj
+++ src/recursion.clj
@@ -72,13 +72,13 @@
     :else (seq a-seq)))
 
 (defn seq= [a-seq b-seq]
-  (cond
-    (and (empty? a-seq)
-         (empty? b-seq)) true
-    (= (first a-seq)
-       (first b-seq)) (seq= (rest a-seq)
-                            (rest b-seq))
-    :else false))
+  (or
+    (and (empty? a-seq) (empty? b-seq))
+    (and
+      (= (first a-seq)
+         (first b-seq))
+      (seq= (rest a-seq)
+            (rest b-seq)))))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
