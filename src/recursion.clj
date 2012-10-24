37261ff7a8971bfe848f28792e9aceadb0e97f30
Antti O P Karjalainen <aopkarja@dhp-27.cs.helsinki.fi>
2012-10-24 16:59:00 +0300

    Fourth one is coming soon...

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,22 +1,49 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (empty? (rest coll))
+       (not (empty? coll))))
 
 (defn my-last [coll]
-  :-)
+  (let [rest-of-coll (rest coll)]
+  (if (empty? rest-of-coll)
+    (first coll)
+    (my-last rest-of-coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+   (empty? a-seq) nil
+   (singleton? a-seq) (first a-seq)
+   :else (max (first a-seq) (max-element (rest a-seq)))))
+
+(defn recursive-seq-max [seq-1 seq-2]
+  (cond
+   (singleton? seq-1) 2
+   (singleton? seq-2) 1
+   :else (recursive-seq-max (rest seq-1) (rest seq-2))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (= (recursive-seq-max seq-1 seq-2) 1)
+    seq-1
+    seq-2))
+
+(defn recursive-longest-sequence [a-seq length b-seq];TODO
+  (let [first1 (first a-seq)
+        rest-of (rest a-seq)
+        count1 (count first1)]
+  (if (singleton? a-seq)
+    b-seq
+    (if (< count1 length)
+      (recursive-longest-sequence rest-of length b-seq)
+      (recursive-longest-sequence rest-of count1 first1)))))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  recursive-longest-sequence [a-seq -1 nil])
 
 (defn my-filter [pred? a-seq]
   [:-])
@@ -89,4 +116,3 @@
 
 (defn powerset [a-set]
   [:-])
-
