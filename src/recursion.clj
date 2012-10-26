d07f10c6c8cf0a0d3fa06400278199f7f485b0f0
Laura Pesola <laura.pesola@helsinki.fi>
2012-10-26 14:53:39 +0300

    ex. 1 - 11

--- src/recursion.clj
+++ src/recursion.clj
@@ -6,34 +6,59 @@
     (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  (and (first coll) (empty? (rest coll))))
+  (and (not (empty? coll)) (empty? (rest coll))))
 
 (defn my-last [coll]
-  (if (singleton? coll) 
-   (first coll)
-   (my-last (rest coll)))
-)
+  (cond
+    (empty? coll) nil
+    (singleton? coll) (first coll)
+    :else (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq) 
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)  
+      (max (first a-seq) (max-element (rest a-seq))))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (cond 
+    (> (count seq-1) (count seq-2)) seq-1
+    :else seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+   nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (seq-max (first a-seq) (longest-sequence (rest a-seq))) )))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+   a-seq
+   (if (pred? (first a-seq))
+     (cons (first a-seq) (my-filter pred? (rest a-seq)))
+     (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond 
+    (empty? a-seq) false
+    (= elem (first a-seq)) true
+    :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+   (if (pred? (first a-seq))
+     (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+     [])))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+   a-seq
+   (if (pred? (first a-seq))
+     (my-drop-while pred? (rest a-seq))
+     a-seq)))
 
 (defn seq= [a-seq b-seq]
   :-)
