fc9d6ee14babc36ee9ab1dc968f1ba58955f15b5
Lassi J Vapaakallio <lvapaaka@dhp-4.cs.helsinki.fi>
2012-10-26 16:32:42 +0300

    kesken

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,40 +1,81 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+   1
+   (* (first coll)
+      (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (if (empty? coll)
+    false
+    (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (cond
+   (empty? coll) nil
+   (singleton? coll) (first coll)
+   :else (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+    (empty? a-seq) nil
+    (singleton? a-seq) (first a-seq) 
+    :else (max (first a-seq) (max-element (rest a-seq)))))
+
+(defn seq1-longer? [seq-1 seq-2]
+  (cond
+   (empty? seq-1) false
+   (empty? seq-2) true
+   :else (seq1-longer? (rest seq-1) (rest seq-2))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (seq1-longer? seq-1 seq-2)
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (cond 
+   (empty? a-seq) nil
+   (singleton? a-seq) (first a-seq)
+   :else (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq)
+            (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+   (empty? a-seq) false
+   (== elem (first a-seq)) true
+   :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond 
+   (empty? a-seq) []
+   (not (pred? (first a-seq))) []
+   :else (cons (first a-seq)
+               (my-take-while pred? (rest a-seq)))))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) []
+   (not (pred? (first a-seq))) a-seq
+   :else (my-drop-while pred? (rest a-seq))))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (= a-seq b-seq))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+   (if (or (empty? seq-1) (empty? seq-2))
+     []
+     (cons
+      (f (first seq-1) (first seq-2)) 
+	  (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
   :-)
@@ -89,4 +130,3 @@
 
 (defn powerset [a-set]
   [:-])
-
