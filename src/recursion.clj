37d6e4d1311440dc1c4d35ffeedb35e2c9ab0d0e
tta <fc6fa0e9@nomail.com>
2012-10-26 22:04:26 +0300

    Kaikki muut tehtävät paitsi bonus-tehtävät.

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,85 +1,140 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (not (empty? coll)) (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? (rest coll))
+    (first coll)
+    (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? (rest a-seq))
+    (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (letfn [(longer? [s1 s2]
+                      (cond (empty? s1) false
+                            (empty? s2) true
+                            :else (longer? (rest s1) (rest s2))))]
+    (if (longer? seq-1 seq-2) seq-1 seq-2)))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? (rest a-seq))
+    (first a-seq)
+    (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond (empty? a-seq) false
+        (= (first a-seq) elem) true
+        :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (if (or (empty? a-seq) (not (pred? (first a-seq)))) '()
+        (cons (first a-seq) (my-take-while pred? (rest a-seq)))))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond (empty? a-seq) '()
+        (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+        :else (cons (first a-seq) (rest a-seq))))
 
 (defn seq= [a-seq b-seq]
-  :-)
+ (if (or (empty? a-seq) (empty? b-seq))
+   (= (empty? a-seq) (empty? b-seq))
+   (seq= (rest a-seq) (rest b-seq))))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+ (if (or (empty? seq-1) (empty? seq-2))
+   '()
+   (cons (f (first seq-1) (first seq-2))
+         (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond (<= n 0) 0
+        (= n 1) 1
+		:else (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (<= how-many-times 0)
+    '()
+	(cons what-to-repeat
+          (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (if (<= up-to 0)
+    '()
+	(cons (dec up-to) (my-range (dec up-to)))))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    (cons '() nil)
+    (cons (seq a-seq) (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (reverse (map reverse (tails (reverse a-seq)))))
 
 (defn rotations [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    (cons '() nil)
+    (rest (map concat (tails a-seq) (inits a-seq)))))
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (my-frequencies-helper
+      (assoc freqs (first a-seq) (inc (or (get freqs (first a-seq)) 0)))
+      (rest a-seq))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (apply concat (map (fn [[v n]] (repeat n v)) a-map)))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (== n 0) (empty? coll))
+    '()
+    (cons (first coll) (my-take (dec n) (rest coll)))))
 
 (defn my-drop [n coll]
-  [:-])
+  (if (or (== n 0) (empty? coll))
+    coll
+    (my-drop (dec n) (rest coll))))
 
 (defn halve [a-seq]
-  [:-])
+  (let [halfcount (int (/ (count a-seq) 2))]
+  [(my-take halfcount a-seq) (my-drop halfcount a-seq)]))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond (empty? a-seq) b-seq
+        (empty? b-seq) a-seq
+        (< (first a-seq) (first b-seq))
+          (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+        :else (cons (first b-seq) (seq-merge a-seq (rest b-seq)))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (or (empty? a-seq) (empty? (rest a-seq)))
+    a-seq
+    (let [[a b] (halve a-seq)]
+      (seq-merge (merge-sort a) (merge-sort b)))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
@@ -89,4 +144,3 @@
 
 (defn powerset [a-set]
   [:-])
-
