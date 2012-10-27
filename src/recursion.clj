0d0b431f127ca0ac140a3a803139ee7d9a334f9c
Juho Gävert <jgavert@cs.helsinki.fi>
2012-10-27 22:43:17 +0300

    asdij

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,72 +1,57 @@
 (ns recursion)
 
-(defn product [coll]
+(defn product [[a & rst :as coll]]
   (if (empty? coll) 1 (* (first coll) (product (rest coll)))))
 
-(defn singleton? [coll]
-  (and (not (empty? coll)) (empty? (rest coll))))
+(defn singleton? [[_ & rst :as coll]]
+  (and (not (empty? coll)) (empty? rst)))
 
-(defn my-last [coll]
-  (if (singleton? coll) (first coll) (if (empty? coll) nil (my-last (rest coll)))))
+(defn my-last [[a & rst :as coll]]
+  (if (singleton? coll) a
+    (if (empty? coll) nil (my-last rst))))
 
-(defn max-element [a-seq]
-  (let [rst (rest (rest a-seq))]
-    (if (singleton? a-seq) (first a-seq)
-      (if (empty? a-seq) nil
-        (max-element (cons (max (first a-seq) (first (rest a-seq))) rst ))))))
+(defn max-element [[a b & rst :as a-seq]]
+  (if (singleton? a-seq) a
+    (if (empty? a-seq) nil
+      (max-element (cons (max a b)) rst ))))
 
 (defn seq-max [seq-1 seq-2]
   (let [fun (fn ! [x y] (if (empty? x) false
                         (if (empty? y) true (! (rest x) (rest y)))))]
     (if (fun seq-1 seq-2) seq-1 seq-2)))
 
-(defn longest-sequence [a-seq]
-  (let [rst (rest (rest a-seq))]
-    (if (singleton? a-seq) (first a-seq)
-      (if (empty? a-seq) nil
-        (longest-sequence (cons (seq-max (first a-seq) (first (rest a-seq))) rst ))))))
+(defn longest-sequence [[a b & rst :as a-seq]]
+  (if (singleton? a-seq) a
+    (if (empty? a-seq) nil
+      (longest-sequence (cons (seq-max a b)) rst ))))
 
 (defn my-filter [pred? [a & rst :as a-seq]]
-    (if (singleton? a-seq)
-      (if (pred? a) [a] [])
-      (if (pred? a) (cons a (my-filter pred? rst)) (my-filter pred? rst))))
+  (if (singleton? a-seq)
+    (if (pred? a) [a] [])
+    (if (pred? a) (cons a (my-filter pred? rst)) (my-filter pred? rst))))
 
-(defn sequence-contains? [elem a-seq]
-  (let [rst (rest a-seq)
-        [a] a-seq]
-    (if (empty? a-seq) false
-      (if (== elem a) true (sequence-contains? elem rst)))))
+(defn sequence-contains? [elem [a & rst :as a-seq]]
+  (if (empty? a-seq) false
+    (if (== elem a) true (sequence-contains? elem rst))))
 
-(defn my-take-while [pred? a-seq]
-  (let [rst (rest a-seq)
-        [a] a-seq]
+(defn my-take-while [pred? [a & rst :as a-seq]]
+  (if (empty? a-seq) []
+    (if (pred? a) (cons a (my-take-while pred? rst)) [])))
+
+(defn my-drop-while [pred? [a & rst :as a-seq]]
     (if (empty? a-seq) []
-      (if (pred? a) (cons a (my-take-while pred? rst)) []))))
+      (if (pred? a) (my-drop-while pred? rst) a-seq)))
 
-(defn my-drop-while [pred? a-seq]
-  (let [rst (rest a-seq)
-        [a] a-seq]
-    (if (empty? a-seq) []
-      (if (pred? a) (my-drop-while pred? rst) a-seq))))
-
-(defn seq= [a-seq b-seq]
-  (let [rst-a (rest a-seq)
-        [a] a-seq
-        rst-b (rest b-seq)
-        [b] b-seq
-        ae? (empty? a-seq)
+(defn seq= [[a & rst-a :as a-seq] [b & rst-b :as b-seq]]
+  (let [ae? (empty? a-seq)
         be? (empty? b-seq)]
     (if (and ae? be?) true
       (if (or ae? be?) false
         (if (== a b) (seq= rst-a rst-b) false)))))
 
-(defn my-map [f seq-1 seq-2]
-  (let [rst-a (rest seq-1)
-        [a] seq-1
-        rst-b (rest seq-2)
-        [b] seq-2
-        ae? (empty? seq-1)
-        be? (empty? seq-2)]
+(defn my-map [f [a & rst-a :as a-seq] [b & rst-b :as b-seq]]
+  (let [ae? (empty? a-seq)
+        be? (empty? b-seq)]
     (if (or ae? be?) []
       (cons (f a b) (my-map f rst-a rst-b)))))
 
