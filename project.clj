1c23eaf1fa4138703d0a9966db3c586aaa620749
Jani Rahkola <jani.rahkola@gmail.com>
2013-03-09 13:42:38 +0200

    updated project

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject recursion "1.0.0-SNAPSHOT"
-  :description "Recursion"
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
-                                  [com.stuartsierra/lazytest "1.2.3"]]
-                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5-RC1"]]
+                   :plugins [[lein-midje "3.0-RC1"]]}})
