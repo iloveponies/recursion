32c3f54cce68f0688d53d1abe805cdd308aa5157
Teemu Patja <tp@iki.fi>
2013-10-22 11:57:30 +0100

    18/102

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,19 +1,40 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if
+      (empty? coll) 1
+      (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and
+   (not (= (first coll) nil)) 
+   (= (first (rest coll)) nil)))
 
 (defn my-last [coll]
-  :-)
+  (cond
+   (empty? coll) nil
+   (empty? (rest coll)) (first coll)
+   :else (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+   (empty? a-seq) nil
+   (empty? (rest a-seq)) (first a-seq)
+   :else (max (first a-seq) (max-element (rest a-seq)))))
+
+(defn longer-seq [seq-1 seq-2]
+  (cond
+   (empty? seq-1) 2
+   (empty? seq-2) 1
+   :else (longer-seq (rest seq-1) (rest seq-2))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (let [longer (longer-seq seq-1 seq-2)]
+    (case longer
+      1 seq-1
+      2 seq-2
+      seq-2)))
+
 
 (defn longest-sequence [a-seq]
   [:-])
