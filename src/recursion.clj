2225274e3719e3992cec9c2c6856b7492fb294c6
root <root@localhost.localdomain>
2013-10-24 16:47:21 +0300

    Viimeiset kolme puuttuu

--- src/recursion.clj
+++ src/recursion.clj
@@ -176,37 +176,119 @@
 ;(my-range 3)  ;=> (2 1 0)
 
 (defn tails [a-seq]
-  [:-])
+ (cond
+  (empty? a-seq) (cons () '())
+  :else (cons (seq a-seq) (tails (rest a-seq)))
+
+  ))
 
 (defn inits [a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)(cons () '())
+   :else (cons (seq a-seq) (inits (reverse(rest (reverse a-seq)))))
+
+   ))
+
+;(tails [1 2 3 4]) ;=> ((1 2 3 4) (2 3 4) (3 4) (4) ())
+;(inits [1 2 3 4]) ;=> (() (1) (1 2) (1 2 3) (1 2 3 4))
+; You can output the tails and inits in any order you like.
+;(inits [1 2 3 4]) ;=> ((1 2) () (1 2 3) (1) (1 2 3 4))
 
 (defn rotations [a-seq]
-  [:-])
+  (letfn [(rotations-helper [seq-1 seq-2]
+            (cond
+             (empty? seq-1) '()
+             :else (cons (concat seq-1 seq-2)(rotations-helper (rest seq-1)(concat seq-2 (take 1 seq-1))))))]
+          (cond
+           (empty? a-seq) '(())
+           :else (rotations-helper a-seq '()))
+    ))
+
+;(rotations [])        ;=> (())
+;(rotations [1 2 3])   ;=> ((1 2 3) (2 3 1) (3 1 2))
+;(rotations [:a :b])   ;=> ((:a :b) (:b :a))
+; The order of rotations does not matter.
+;(rotations [:a :b])   ;=> ((:b :a) (:a :b))
+;(rotations [1 5 9 2]) ;=> ((1 5 9 2) (2 1 5 9) (9 2 1 5) (5 9 2 1))
+;(count (rotations [6 5 8 9 2])) ;=> 5
+
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) freqs
+   :else (let [elem (first a-seq)
+               elem-count (if (contains? freqs elem) (inc (get freqs elem))1)]
+           (my-frequencies-helper (assoc freqs elem elem-count)(rest a-seq)))
+   ))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
+
+;(my-frequencies []) ;=> {}
+;(my-frequencies [:a "moi" :a "moi" "moi" :a 1])
 
 (defn un-frequencies [a-map]
-  [:-])
+  (cond
+   (empty? a-map) '()
+   :else (concat (repeat ((first a-map)1)((first a-map)0))(un-frequencies (rest a-map)))
+   ))
+
+;(un-frequencies {:a 3 :b 2 "^_^" 1})             ;=> (:a :a :a "^_^" :b :b)
+;(un-frequencies (my-frequencies [:a :b :c :a]))  ;=> (:a :a :b :c)
+;(my-frequencies (un-frequencies {:a 100 :b 10})) ;=> {:a 100 :b 10}
 
 (defn my-take [n coll]
-  [:-])
+  (cond
+   (< n 1) '()
+   (empty? coll) '()
+   :else (cons (first coll)(my-take (dec n) (rest coll)))
+   ))
+
+;(my-take 2 [1 2 3 4]) ;=> (1 2)
+;(my-take 4 [:a :b])   ;=> (:a :b)
 
 (defn my-drop [n coll]
-  [:-])
+  (cond
+   (empty? coll) '()
+   (< n 1) (cons (first coll)(my-drop (dec n) (rest coll)))
+   :else (my-drop (dec n) (rest coll))
+
+   ))
+
+;(my-drop 2 [1 2 3 4]) ;=> (3 4)
+;(my-drop 4 [:a :b])   ;=> ()
 
 (defn halve [a-seq]
-  [:-])
+  (let [a (int (/ (count a-seq) 2))]
+    (vector (take a a-seq) (drop a a-seq))))
+
+;(halve [1 2 3 4])   ;=> [(1 2) (3 4)]
+;(halve [1 2 3 4 5]) ;=> [(1 2) (3 4 5)]
+;(halve [1])         ;=> [() (1)]
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond
+   (empty? a-seq) b-seq
+   (empty? b-seq) a-seq
+   (< (first a-seq) (first b-seq)) (cons (first a-seq)(seq-merge (rest a-seq) b-seq))
+   :else (cons (first b-seq)(seq-merge (rest b-seq) a-seq))
+
+
+   ))
+
+;(seq-merge [4] [1 2 6 7])        ;=> (1 2 4 6 7)
+;(seq-merge [1 5 7 9] [2 2 8 10]) ;=> (1 2 2 5 7 8 9 10)
 
 (defn merge-sort [a-seq]
-  [:-])
+  (let [allthosehalves (halve a-seq)]
+    (cond
+     (<= (count (allthosehalves 1))1) (seq-merge (allthosehalves 1)(allthosehalves 0))
+     :else (seq-merge (merge-sort (allthosehalves 0))(merge-sort (allthosehalves 1)))
+     )))
+
+;(merge-sort [])                 ;=> ()
+;(merge-sort [1 2 3])            ;=> (1 2 3)
+;(merge-sort [5 3 4 17 2 100 1]) ;=> (1 2 3 4 5 17 100)
 
 (defn split-into-monotonics [a-seq]
   [:-])
