0588c5a06482ad27de5f3fbf7ed97803e745ebf8
jeto <Jere@host-003.elisa>
2013-10-24 20:55:35 +0300

    kesken

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,40 +1,68 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll) 1
+  (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (cond
+   (empty? coll) false
+   (not (empty? (rest coll))) false
+   :else true))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? coll) nil
+    (if (empty? (rest coll)) (first coll)
+      (my-last (rest coll)))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq) nil
+    (if (empty? (rest a-seq)) (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq))))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1)(count seq-2)) seq-1 seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq) nil
+    (if (empty? (rest a-seq)) (first a-seq)
+      (seq-max (first a-seq) (longest-sequence (rest a-seq))))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq)) (cons (first a-seq) (my-filter pred? (rest a-seq)))
+   (not (pred? (first a-seq))) (my-filter pred? (rest a-seq))
+   :else '()))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+   (empty? a-seq) false
+   (== elem (first a-seq)) true
+   :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq)) (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+   :else '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+   :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+   (and (empty? a-seq)(empty? b-seq)) true
+   (or (and (empty? a-seq)(not (empty? b-seq)))
+       (and (not (empty? a-seq))(empty? b-seq))) false
+   (== (first a-seq)(first b-seq)) (seq= (rest a-seq)(rest b-seq))
+   :else false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  )
 
 (defn power [n k]
   :-)
