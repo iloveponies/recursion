8b6ce31d150c4c99b2438441e85ec37872cf1de9
Teemu Sarapisto <tsarpf@gmail.com>
2013-10-25 09:17:52 +0300

    rotations done

--- src/recursion.clj
+++ src/recursion.clj
@@ -41,53 +41,93 @@
       (seq-max (first a-seq) (longest-sequence(rest a-seq))))))
 
 (defn my-filter [pred? a-seq]
-  (if
-    (empty? (rest a-seq))
-    (if
-      (pred? (first a-seq))
-      (first a-seq)
-      nil)
-    (if
-      (pred? (first a-seq))
-      cons (first a-seq) (my-filter pred? (rest a-seq))
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
       (my-filter pred? (rest a-seq)))))
 
-
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+   (empty? a-seq) false
+   (= (first a-seq) elem) true
+   :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq))(cons (first a-seq) (my-take-while pred? (rest a-seq)))
+   :else '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+   :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+   (and (empty? a-seq) (empty? b-seq)) true
+   (and (empty? a-seq) (not (empty? b-seq))) false
+   (and (not (empty? a-seq)) (empty? b-seq)) false
+   (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))
+   :else false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (cond
+   (or (empty? seq-1) (empty? seq-2)) '()
+   :else (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if
+    (zero? k)
+    1
+    (* n (power n (- k 1)))))
 
 (defn fib [n]
-  :-)
+  (cond
+   (== 0 n) 0
+   (== 1 n) 1
+   :else (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (cond
+   (zero? how-many-times) '()
+   (neg? how-many-times) '()
+   :else (cons what-to-repeat (my-repeat (- how-many-times 1) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (cond
+   (zero? up-to) '()
+   :else (cons (- up-to 1) (my-range (- up-to 1)))))
 
 (defn tails [a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) (cons '() a-seq)
+   :else (cons a-seq (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (map reverse (tails (reverse a-seq))))
+  ;(map reverse (reverse (tails (reverse a-seq)))))
 
 (defn rotations [a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) '(())
+   :else (drop-last (map reverse (map concat (map reverse (inits a-seq)) (reverse (map reverse (tails a-seq))))))))
+
+(defn count-elem-helper [n elem coll]
+  (if (empty? coll)
+    n
+    (let [new-count (if (= elem (first coll))
+                      (inc n)
+                      n)]
+      (count-elem-helper new-count
+                         elem
+                         (rest coll)))))
+
+(defn count-elem [elem coll]
+    (count-elem-helper 0 elem coll))
+
 
 (defn my-frequencies-helper [freqs a-seq]
   [:-])
