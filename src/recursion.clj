c117a0b5f53032d2b641aae773d521a4228ff1f9
Payel <pbandyop@cs.helsinki.fi>
2013-10-25 14:05:42 +0300

    updated recursion exercise submission

--- src/recursion.clj
+++ src/recursion.clj
@@ -40,40 +40,88 @@
 
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (if
+   (empty? a-seq)
+     false
+   (if (= (first a-seq) elem)
+       true
+        (sequence-contains? elem (rest a-seq))
+         )))
+
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (if
+   (or (empty? a-seq) (= pred? even?))
+     (list )
+  (if
+   (= pred? odd?)
+
+   (if (integer? (/ (second a-seq) 2))
+       (list (first a-seq)) (list (first a-seq) (second a-seq))
+         ) false))
+  )
+
+
+  (defn mmap [f a-seq]
+  (if (empty? a-seq)
+    ()
+    (cons (f (first a-seq))
+          (mmap f (rest a-seq)))))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) ()
+    (= (first (mmap pred? a-seq)) false) (seq a-seq)
+    :else (my-drop-while pred? (rest a-seq))))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+       (= a-seq b-seq) true
+       :else false)
+      )
+
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+     (list )
+            (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2))
+      )))
 
-(defn power [n k]
-  :-)
+
+(defn power [n k] (int (Math/pow n k)))
+
 
 (defn fib [n]
-  :-)
+  (cond
+   (= n 0) 0
+    (= n 1) 1
+    :else (+ (fib (- n 1)) (fib (- n 2)))))
 
-(defn my-repeat [how-many-times what-to-repeat]
-  [:-])
 
-(defn my-range [up-to]
-  [:-])
+(defn my-repeat [how-many-times what-to-repeat] (take how-many-times (cycle [what-to-repeat])))
+
+
+(defn my-range [up-to] (reverse (range up-to)))
+
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+           '(())
+           (cons (seq a-seq) (tails (rest a-seq)))))
 
-(defn inits [a-seq]
-  [:-])
+
+(defn inits [a-seq] (map reverse (tails (reverse a-seq))))
+
+(defn in [a-seq] (tails (reverse a-seq)))
 
 (defn rotations [a-seq]
-  [:-])
+  (cond
+   (= a-seq []) (list (map concat (reverse (rest (tails a-seq))) (map reverse (in a-seq))))
+
+    :else (map concat (reverse (rest (tails a-seq))) (map reverse (in a-seq)))
+
+   ))
+
 
 (defn my-frequencies-helper [freqs a-seq]
   [:-])
@@ -85,13 +133,18 @@
   [:-])
 
 (defn my-take [n coll]
-  [:-])
+  (cond
+   (> n (count coll)) (seq coll)
+   :else (reverse (drop n (reverse coll)))
+   )
+  )
 
-(defn my-drop [n coll]
-  [:-])
 
-(defn halve [a-seq]
-  [:-])
+(defn my-drop [n coll] (drop n coll))
+
+
+(defn halve [a-seq] (vector (my-take  (- (count a-seq) (int (/ (count a-seq) 2))) a-seq) (my-drop (int (/ (count a-seq) 2)) a-seq)) )
+
 
 (defn seq-merge [a-seq b-seq]
   [:-])
@@ -107,4 +160,6 @@
 
 (defn powerset [a-set]
   [:-])
+
+
 
