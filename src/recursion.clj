2802fecf031195a244c24e21c961175504238fb1
Aleksi Paavola <ahpaavola@gmail.com>
2013-10-26 19:38:12 +0300

    recursion, all tests except encore passed

--- src/recursion.clj
+++ src/recursion.clj
@@ -127,36 +127,81 @@
                (map (fn [x] (cons (first a-seq) x))
                      (inits (rest a-seq))))))
 
+; gotta go fast
+(defn giga-rotator-300 [n a-seq]
+  (cond
+   (= 1 n ) [a-seq]
+   :else   (cons
+            a-seq
+            (giga-rotator-300
+             (dec n)
+             (concat
+              (rest a-seq)
+              [(first a-seq)])))))
+
 (defn rotations [a-seq]
   (cond
-   (empty? a-seq) []
-   :else (map (fn [x] (cons (rest a-seq)
-                            (first a-seq)))
-                      (rotations (rest a-seq)))))
+   (empty? a-seq) [[]]
+   :else (giga-rotator-300 (count a-seq) a-seq)))          ;=> Here we go!!
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (let [new-count (if (contains? freqs (first a-seq))
+                      (assoc freqs (first a-seq) (inc (get freqs (first a-seq))))
+                      (merge freqs {(first a-seq) 1}))]
+      (my-frequencies-helper new-count (rest a-seq))
+      )))
+
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (if (empty? a-map)
+    a-map
+    (let [repeater (take (get (first a-map) 1)
+          (repeat (get (first a-map) 0)))]
+      (concat repeater (un-frequencies (rest a-map)))
+      )))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (empty? coll) (= 0 n))
+  ()
+  (cons (first coll)
+        (my-take
+         (dec n)
+         (rest coll)))))
 
 (defn my-drop [n coll]
-  [:-])
+  (if (or (empty? coll) (= 0 n))
+    coll
+    (my-drop (dec n) (rest coll))))
 
 (defn halve [a-seq]
-  [:-])
+  (let [half-of-set (int (/ (count a-seq) 2))]
+    [(my-take half-of-set
+              a-seq)
+     (my-drop half-of-set
+              a-seq)]))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond
+   (empty? a-seq) b-seq
+   (empty? b-seq) a-seq
+   :else
+   (let [x (first a-seq)
+         y (first b-seq)]
+     (if (< x y )
+       (cons x (seq-merge (rest a-seq) b-seq))
+       (cons y (seq-merge (rest b-seq) a-seq))))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (> 2 (count a-seq))
+    a-seq
+    (let [[x y] (halve a-seq)]
+      (seq-merge (merge-sort x) (merge-sort y)))
+   ))
 
 (defn split-into-monotonics [a-seq]
   [:-])
