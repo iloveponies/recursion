3a3874df3b5cc371d49eff6146198ff23c7e37b4
Mikael <->
2013-10-27 12:24:18 +0200

    .

--- src/recursion.clj
+++ src/recursion.clj
@@ -31,13 +31,27 @@
        (first coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+   (empty? a-seq)
+   nil
+   (singleton? a-seq)
+    (first a-seq)
+  :else
+   (max (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)
+   nil
+   (singleton? a-seq)
+   (first a-seq)
+   :else
+   (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
   (if (empty? a-seq)
@@ -75,6 +89,8 @@
   (cond
    (and (empty? a-seq) (empty? b-seq))
    true
+   (or (empty? a-seq) (empty? b-seq))
+   false
    (= (first a-seq) (first b-seq))
    (seq= (rest a-seq) (rest b-seq))
    :else false))
@@ -87,14 +103,16 @@
         (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  (if (zero? n)
-    0
-    (if (zero? k)
-      1
-    (* n (power n (dec k))))))
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (if (zero? n)
+    0
+    (if (= n 1)
+      1
+      (+ (fib (- n 1)) (fib (- n 2))))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   (if (<= how-many-times 0)
@@ -113,19 +131,29 @@
           (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (map reverse (tails (reverse a-seq))))
 
 (defn rotations [a-seq]
-  [:-])
+  (set(map concat (tails a-seq) (reverse(inits a-seq)))))
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (if (contains? freqs (first a-seq))
+      (my-frequencies-helper
+       (assoc freqs (first a-seq) (inc (get freqs (first a-seq)))) (rest a-seq))
+      (my-frequencies-helper
+       (assoc freqs (first a-seq) 1) (rest a-seq)))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+    (if (empty? a-map)
+      ()
+      (let [times (second (first a-map))]
+      (concat (repeat times (first (first a-map)))
+      (un-frequencies (rest a-map))))))
 
 (defn my-take [n coll]
   (if (zero? n)
@@ -142,13 +170,31 @@
       ())))
 
 (defn halve [a-seq]
-  [:-])
+  [(my-take (int (/ (count a-seq) 2)) a-seq)
+  (my-drop (int (/ (count a-seq) 2)) a-seq)])
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (if (empty? a-seq)
+    b-seq
+    (if (empty? b-seq)
+      a-seq
+      (if (< (first a-seq) (first b-seq))
+        (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+        (cons (first b-seq) (seq-merge a-seq (rest b-seq)))))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (or
+       (empty? a-seq)
+       (singleton? a-seq))
+    a-seq))
+
+(defn merge-sort [a-seq]
+  (if (or
+       (empty? a-seq)
+       (singleton? a-seq))
+    a-seq
+    (let [halfs (halve a-seq)]
+    (seq-merge (merge-sort (first halfs)) (merge-sort (second halfs))))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
