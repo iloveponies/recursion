89e382a76be1f82de697dddaae75fa58a98ca285
Lenni <lenni@live.com>
2013-10-27 23:10:22 +0200

    7 tehtavaa tehtyna, enempaa ei jaksa enaa

--- src/recursion.clj
+++ src/recursion.clj
@@ -61,7 +61,14 @@
 
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq))
+     (cons (first a-seq) (my-filter pred? (rest a-seq))
+   )
+   :else (my-filter pred? (rest a-seq))
+  )
+)
 
 (defn sequence-contains? [elem a-seq]
   :-)
