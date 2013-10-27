d1e819663bbec44a375203e7b0395cfc6da7c9ee
mxO <miro.sorja@gmail.com>
2013-10-27 13:43:22 +0200

    Done

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,22 +1,59 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+
+  (if (empty? coll)
+    1
+  (* (first coll)
+     (product (rest coll))
+  ))
+  )
 
 (defn singleton? [coll]
-  :-)
+   (if (empty? coll)
+    false
+    (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? coll)
+    nil
+    (if (singleton? coll)
+      (first coll)
+      (my-last (rest coll))
+      )
+    )
+  )
 
 (defn max-element [a-seq]
-  :-)
+
+  (if (empty? a-seq)
+  nil
+  (if (singleton? a-seq)
+    (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq)))
+    )))
+
+
+
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (let [length-1 (count seq-1)
+        length-2 (count seq-2)]
+   (if (> length-1 length-2)
+     seq-1
+     seq-2
+     )
+   )
+  )
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+  nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (seq-max
+       (first a-seq)
+       (longest-sequence (rest a-seq))))))
 
 (defn my-filter [pred? a-seq]
   [:-])
