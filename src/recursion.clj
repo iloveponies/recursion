615d6e0e1e560decee1c3313de566c3641c50dba
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-02-02 13:43:36 -0500

    WIP: 27 of 29 exercises complete

--- src/recursion.clj
+++ src/recursion.clj
@@ -218,25 +218,148 @@
   (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (mapcat (fn [[k v]] (repeat v k)) a-map))
 
 (defn my-take [n coll]
-  [:-])
+  (loop [n n
+         coll coll
+         new-coll (empty coll)]
+    (if (or (empty? coll) (= n 0))
+      new-coll
+      (recur (dec n) (rest coll) (conj new-coll (first coll))))))
 
 (defn my-drop [n coll]
-  [:-])
+  (if (or (empty? coll) (= n 0))
+    coll
+    (recur (dec n) (rest coll))))
 
+;; Returns vector of two list-like structures, not vectors
+(defn halve-list [a-seq]
+  (let [midpoint (int (/ (count a-seq) 2))]
+    [(apply list (my-take midpoint a-seq)) (my-drop midpoint a-seq)]))
+
+;; Returns vector of two vectors
 (defn halve [a-seq]
-  [:-])
+  (let [midpoint (int (/ (count a-seq) 2))]
+    [(my-take midpoint a-seq) (vec (my-drop midpoint a-seq))]))
 
+(comment
+  ;; Helper function for tail-recursive version, expects lists
+  (defn next-seqs-in-merge [a-seq b-seq m-seq]
+    (let [a-val (first a-seq)
+          b-val (first b-seq)]
+      (if (< a-val b-val)
+        [(rest a-seq) b-seq (conj m-seq a-val)]
+        [a-seq (rest b-seq) (conj m-seq b-val)]))))
+
+(comment
+  ;; Tail-recursive, expects lists
+  (defn seq-merge [a-seq b-seq]
+    (loop [a-seq a-seq
+           b-seq b-seq
+           m-seq ()]
+      (cond (empty? a-seq) (concat m-seq b-seq)
+            (empty? b-seq) (concat m-seq a-seq)
+            :else (let [next-seqs (next-seqs-in-merge a-seq b-seq m-seq)
+                        [new-a-seq new-b-seq new-m-seq] next-seqs]
+                    (recur new-a-seq new-b-seq new-m-seq))))))
+
+;; Helper function for tail-recursive version, expects vectors
+(defn next-seqs-in-merge [a-seq b-seq m-seq]
+  (let [a-val (peek a-seq)
+        b-val (peek b-seq)]
+    (if (> a-val b-val)
+      [(pop a-seq) b-seq (conj m-seq a-val)]
+      [a-seq (pop b-seq) (conj m-seq b-val)])))
+
+;; Tail-recursive, expects vectors
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (loop [a-seq a-seq
+         b-seq b-seq
+         m-seq ()]
+    (cond (empty? a-seq) (vec (concat b-seq m-seq))
+          (empty? b-seq) (vec (concat a-seq m-seq))
+          :else (let [next-seqs (next-seqs-in-merge a-seq b-seq m-seq)
+                      [new-a-seq new-b-seq new-m-seq] next-seqs]
+                  (recur new-a-seq new-b-seq new-m-seq)))))
+
+(comment
+  ;; Not tail-recursive, expects lists
+  (defn seq-merge [a-seq b-seq]
+    (let [a-val (first a-seq)
+          b-val (first b-seq)]
+      (cond (empty? a-seq) b-seq
+            (empty? b-seq) a-seq
+            (< a-val b-val) (conj (seq-merge (rest a-seq) b-seq) a-val)
+            :else (conj (seq-merge a-seq (rest b-seq)) b-val)))))
+
+;; Not tail-recursive, expects vectors
+(defn seq-merge [a-seq b-seq]
+  (let [a-val (peek a-seq)
+        b-val (peek b-seq)]
+    (cond (empty? a-seq) b-seq
+          (empty? b-seq) a-seq
+          (> a-val b-val) (conj (seq-merge (pop a-seq) b-seq) a-val)
+          :else (conj (seq-merge a-seq (pop b-seq)) b-val))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (<= (count a-seq) 1)
+    a-seq
+    (let [[seq-1 seq-2] (halve a-seq)]
+      (seq-merge (merge-sort seq-1) (merge-sort seq-2)))))
+
+;; Helper function for "recur version" mono-chop
+(defn mono-iter [mono-seq a-seq f]
+  (if (empty? a-seq)
+    [mono-seq []]
+    (let [a-val (last mono-seq)
+          b-val (first a-seq)]
+      (if (not (f a-val b-val))
+        [mono-seq a-seq]
+        (recur (conj mono-seq b-val) (rest a-seq) f)))))
+
+;; Tail-recursive version
+(defn mono-chop [a-seq]
+  (if (<= (count a-seq) 2)
+    [a-seq []]
+    (let [a-val (first a-seq)
+          b-val (second a-seq)
+          f (if (< a-val b-val)
+              <=
+              >=)]
+      (mono-iter [a-val b-val] (rest (rest a-seq)) f))))
+
+;; Helper function for "reduce version" mono-chop
+(defn mono-step [f [mono-seq b-seq] next-item]
+  (let [prev-item (last mono-seq)
+        continue (and (empty? b-seq) (f prev-item next-item))]
+    (cond continue [(conj mono-seq next-item) b-seq]
+          :else [mono-seq (conj b-seq next-item)])))
+
+;; Uses reduce instead of recur
+;; Returns a two element vector
+;; The first element is the monotonic sequence at the head of the
+;; original sequence, the second element is the rest of the original
+;; sequence without the beginning monotonic sequence
+;; Returns [mono-seq rest-a-seq] such that
+;; (= (concat mono-seq rest-a-seq) a-seq)
+(defn mono-chop [a-seq]
+  (if (<= (count a-seq) 2)
+    [a-seq []]
+    (let [a-val (first a-seq)
+          b-val (second a-seq)
+          f (if (< a-val b-val)
+              <=
+              >=)
+          mono-step (partial mono-step f)]
+      (reduce mono-step [[a-val b-val] []] (rest (rest a-seq))))))
 
 (defn split-into-monotonics [a-seq]
-  [:-])
+  (loop [a-seq a-seq monos []]
+    (if (empty? a-seq)
+      monos
+      (let [[b-seq c-seq] (mono-chop a-seq)]
+        (recur c-seq (conj monos b-seq))))))
 
 (defn permutations [a-set]
   [:-])
