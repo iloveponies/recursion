7a63a9de5e5b772c8dbe5885a59d82adcd242f4d
Turjo Tuohiniemi <turjo.tuohiniemi@reaktor.fi>
2014-05-05 20:21:39 +0300

    Lisää kamaa

--- src/harjoittelua.clj
+++ src/harjoittelua.clj
@@ -18,3 +18,84 @@
 (max-element [2 4 1 4]) ;=> 4
 (max-element [2])       ;=> 2
 (max-element [])        ;=> nil
+
+(seq-max [1] [1 2])   ;=> [1 2]
+(seq-max [1 2] [3 4]) ;=> [3 4]
+
+(longest-sequence [[1 2] [] [1 2 3]]) ;=> [1 2 3]
+(longest-sequence [[1 2]])            ;=> [1 2]
+(longest-sequence [])                 ;=> nil
+(longest-sequence [[1 2] [1 2 3] [1 2 3 4] [1 2 3 4 5] [1]])
+(longest-sequence [[1] [1 2] [1 2 3]]) ;=> [1 2 3]
+
+(my-filter odd? [1 2 3 4]) ;=> (1 3)
+(my-filter (fn [x] (> x 9000)) [12 49 90 9001]) ;=> (9001)
+(my-filter even? [1 3 5 7]) ;=> ()
+
+(sequence-contains? 3 [1 2 3]) ;=> true
+(sequence-contains? 3 [4 7 9]) ;=> false
+(sequence-contains? :pony [])  ;=> false
+
+(my-take-while odd? [1 2 3 4])  ;=> (1)
+(my-take-while odd? [1 3 4 5])  ;=> (1 3)
+(my-take-while even? [1 3 4 5]) ;=> ()
+(my-take-while odd? [])         ;=> ()
+
+(my-drop-while odd? [1 2 3 4])  ;=> (2 3 4)
+(my-drop-while odd? [1 3 4 5])  ;=> (4 5)
+(my-drop-while even? [1 3 4 5]) ;=> (1 3 4 5)
+(my-drop-while odd? [])         ;=> ()
+
+(seq= [1 2 4] '(1 2 4))  ;=> true
+(seq= [1 2 3] [1 2 3 4]) ;=> false
+(seq= [1 3 5] [])        ;=> false
+
+(my-map + [1 2 3] [4 4 4])   ;=> (5 6 7)
+(my-map + [1 2 3 4] [0 0 0]) ;=> (1 2 3)
+(my-map + [1 2 3] [])        ;=> ()
+
+(power 2 2)  ;=> 4
+(power 5 3)  ;=> 125
+(power 7 0)  ;=> 1
+(power 0 10) ;=> 0
+(power 2 -2)
+
+(fib 0) ;=> 0
+(fib 1) ;=> 1
+(fib 2) ;=> 1
+(fib 3) ;=> 2
+(fib 4) ;=> 3
+(fib 5) ;=> 5
+(fib 6) ;=> 8
+;...
+(fib 10) ;=> 55
+
+(my-repeat 2 :a)    ;=> (:a :a)
+(my-repeat 3 "lol") ;=> ("lol" "lol" "lol")
+(my-repeat -1 :a)   ;=> ()
+
+(my-range 0)  ;=> ()
+(my-range 1)  ;=> (0)
+(my-range 2)  ;=> (1 0)
+(my-range 3)  ;=> (2 1 0)
+
+(tails [1 2 3 4]) ;=> ((1 2 3 4) (2 3 4) (3 4) (4) ())
+(inits [1 2 3 4]) ;=> ((1 2) () (1 2 3) (1) (1 2 3 4))
+
+(rotations [])        ;=> (())
+(rotations [1 2 3])   ;=> ((1 2 3) (2 3 1) (3 1 2))
+(rotations [:a :b])   ;=> ((:a :b) (:b :a))
+; The order of rotations does not matter.
+(rotations [:a :b])   ;=> ((:b :a) (:a :b))
+(rotations [1 5 9 2]) ;=> ((1 5 9 2) (2 1 5 9) (9 2 1 5) (5 9 2 1))
+(count (rotations [6 5 8 9 2])) ;=> 5
+
+(my-frequencies []) ;=> {}
+(my-frequencies [:a "moi" :a "moi" "moi" :a 1]) ;=> {:a 3, "moi" 3, 1 1}
+(un-frequencies {:a 3, "moi" 3, 1 1})
+
+
+
+
+
+
--- src/recursion.clj
+++ src/recursion.clj
@@ -16,69 +16,142 @@
       (first coll)
       (my-last restcoll))))
 
-;(defn max-element [a-seq]
-;  (if (or (singleton? a-seq) (empty? a-seq))
-;    (first a-seq)
-;    (max (first a-seq) (max-element (rest a-seq)))))
 (defn max-element [a-seq]
-  (if (empty? a-seq)
-    nil
+  (if (or (singleton? a-seq) (empty? a-seq))
+    (first a-seq)
     (max (first a-seq) (max-element (rest a-seq)))))
-
+;(defn max-element [a-seq]
+;  (if (empty? a-seq)
+;    nil
+;    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    nil
+    (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+   (empty? a-seq)
+     false
+   (= (first a-seq) elem)
+     true
+   :else
+     (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)
+     a-seq
+   (pred? (first a-seq))
+     (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+   :else
+     '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)
+     a-seq
+   (pred? (first a-seq))
+     (my-drop-while pred? (rest a-seq))
+   :else
+     a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+   (and (empty? a-seq) (empty? b-seq))
+     true
+   (= (first a-seq) (first b-seq))
+     (seq= (rest a-seq) (rest b-seq))
+   :else
+     false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+    '()
+    (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))
+   ))
 
 (defn power [n k]
-  :-)
+  (cond
+   (< k 0)
+     (/ 1 (power n (* k -1)))
+   (= k 0)
+     1
+   :else
+     (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+   (= n 0)
+     0
+   (= n 1)
+     1
+   :else
+     (+ (fib (dec n)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (<= how-many-times 0)
+    '()
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (if (<= up-to 0)
+    '()
+    (cons (dec up-to) (my-range (dec up-to)))))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '(())
+    (cons a-seq (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (let [reverse-list (fn [list] (reverse list))]
+    (map reverse-list (tails (reverse a-seq)))))
+
+(defn first-n [a-seq n]
+  (if (= n 0)
+    '()
+    (cons (first a-seq) (first-n (rest a-seq) (dec n)))))
+
+(defn rest-n [a-seq n]
+  (if (= n 0)
+    a-seq
+    (rest-n (rest a-seq) (dec n))))
 
 (defn rotations [a-seq]
-  [:-])
+  (let [rotate-seq (fn [n] (concat (rest-n a-seq n) (first-n a-seq n) ))]
+    (map rotate-seq (range (count a-seq)))))
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (let [first-item (first a-seq)]
+      (my-frequencies-helper (assoc freqs first-item (inc (get freqs first-item 0))) (rest a-seq)))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
+
+(defn un-frequencies2 [a-map]
+  (flatten (map (fn [k] (repeat (get a-map k) k)) (keys a-map))))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (let [first-item (first a-map)]
+    (if (empty? a-map)
+      '()
+      (concat (repeat (get first-item 1) (get first-item 0)) (un-frequencies (rest a-map))))))
 
 (defn my-take [n coll]
   [:-])
