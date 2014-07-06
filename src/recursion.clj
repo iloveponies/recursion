b4417e56d6e812328a391f983a74871b8a4edac6
David Hovemeyer <david.hovemeyer@gmail.com>
2014-07-06 12:55:12 -0400

    Eli implemented the longest-sequence function

--- src/recursion.clj
+++ src/recursion.clj
@@ -28,7 +28,10 @@
       seq-2)))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) nil
+    (singleton? a-seq) (first a-seq)
+    :else (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
   [:-])
