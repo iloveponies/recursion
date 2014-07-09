43b3ca9ff5ecfc457a9549ed1b0d55d2145bb298
Tim Steinbach <tsteinbach@trustwave.com>
2014-07-09 12:56:13 -0400

    Ex 5

--- src/recursion.clj
+++ src/recursion.clj
@@ -15,7 +15,10 @@
     :else (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+    (= [] a-seq) nil
+    (singleton? a-seq) (first a-seq)
+    :else (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
   [:-])
