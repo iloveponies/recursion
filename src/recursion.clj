22383ffc8196f95add39a98f01aa1b5dd2adb94f
Tim Steinbach <tsteinbach@trustwave.com>
2014-07-10 14:34:06 -0400

    Ex 15

--- src/recursion.clj
+++ src/recursion.clj
@@ -71,7 +71,10 @@
     :else (* n (power n (- k 1)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (= 0 n) 0
+    (= 1 n) 1
+    :else (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
