4daf5f1296abbdd88c3917f8932d22702d9968d2
Tim Steinbach <tsteinbach@trustwave.com>
2014-07-10 13:16:49 -0400

    Ex 12

--- src/recursion.clj
+++ src/recursion.clj
@@ -54,7 +54,11 @@
     :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and (empty? a-seq) (empty? b-seq)) true
+    (or (empty? a-seq) (empty? b-seq)) false
+    (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))
+    :else false))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
