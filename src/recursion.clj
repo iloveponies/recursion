4daef97bd80b2712d57e22a3b59596bbba1547f3
Colin Bell <col@baibell.org>
2014-07-13 09:37:04 +1000

    Fix singleton?

--- src/recursion.clj
+++ src/recursion.clj
@@ -6,7 +6,7 @@
     (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  (and (not (nil? (first coll))) (empty? (rest coll))))
+  (and (not= [] (first coll)) (empty? (rest coll))))
 
 (defn my-last [coll]
   (if (empty? coll)
