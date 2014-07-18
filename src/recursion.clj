86094c00b841deeaf0c1e6ed9af05f26f71937be
Matteo Traina <matteo.traina.mail@gmail.com>
2014-07-18 10:44:41 +0100

    second

--- src/recursion.clj
+++ src/recursion.clj
@@ -25,7 +25,10 @@
   (if (empty-or-singleton? a-seq) (first a-seq) (seq-max (first a-seq)(longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq) a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq)(my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
   :-)
