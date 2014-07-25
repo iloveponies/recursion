7a8caf023802f41133f8d6536d57db84b9278c2a
madHamish42 <pe_di@gmx.de>
2014-07-25 21:18:15 +0200

    after ex15

--- src/recursion.clj
+++ src/recursion.clj
@@ -59,16 +59,27 @@
    :else (my-drop-while pred? (rest a-seq))))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+   (and (empty? a-seq) (empty? b-seq)) true
+   (not= (count a-seq) (count b-seq)) false
+   (not= (first a-seq) (first b-seq)) false
+   :else (seq= (rest a-seq) (rest b-seq))))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2)) '()
+      (cons (f (first seq-1) (first seq-2))
+            (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (if (< n 2)
+    n
+    (+ (fib (- n 1))
+       (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
@@ -119,4 +130,5 @@
   [:-])
 
 (use 'clojure.repl)
+
 
