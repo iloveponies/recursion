2c49194b909432146de1754d9e9f15e19f6dedcb
zeroDivisible <zerodivisible@gmail.com>
2014-07-26 19:48:22 +0100

    implemented up to ex 11

--- src/recursion.clj
+++ src/recursion.clj
@@ -46,10 +46,19 @@
     :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    (pred? (first a-seq)) (cons
+                            (first a-seq)
+                            (my-take-while pred? (rest a-seq)))
+    :else (empty a-seq)))
+
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+    :else a-seq))
 
 (defn seq= [a-seq b-seq]
   :-)
