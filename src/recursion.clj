0a2008fe5f522464bb8eb91cc518d11fc96ae45f
Alex Podaras <apodaras@gmail.com>
2014-08-17 20:35:00 +0300

    Solve exercise 11

--- src/recursion.clj
+++ src/recursion.clj
@@ -54,7 +54,10 @@
      :else          '())))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)        a-seq
+   (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+   :else                 a-seq))
 
 (defn seq= [a-seq b-seq]
   :-)
