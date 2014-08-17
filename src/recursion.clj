07a3fc29d5a1794818024c98e2c6bf6bc093b065
Alex Podaras <apodaras@gmail.com>
2014-08-17 16:51:58 +0300

    Solve exercise 5

--- src/recursion.clj
+++ src/recursion.clj
@@ -15,7 +15,9 @@
     (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond (empty? a-seq)     nil
+        (singleton? a-seq) (first a-seq)
+        :else              (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
   [:-])
