575620675004364dcbeb0f02ac9a96da12505693
Alex Podaras <apodaras@gmail.com>
2014-08-19 23:14:26 +0300

    Solve exercise 22

--- src/recursion.clj
+++ src/recursion.clj
@@ -133,7 +133,10 @@
               (un-frequencies (rest a-map))))))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (zero? n) (empty? coll))
+    '()
+    (cons (first coll)
+          (my-take (dec n) (rest coll)))))
 
 (defn my-drop [n coll]
   [:-])
