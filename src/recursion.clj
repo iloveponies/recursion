5c4ac7166a74c973d2ed25cb41f74b986510c646
Teemu Tuulari <teemu.tuulari@greyarealabs.com>
2014-08-20 20:44:09 +0300

    Started.

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,37 +1,87 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+ (if (empty? coll)
+   1
+   (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+ (and
+   (not (empty? coll))
+   (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+ (if (empty? coll)
+   nil
+   (if (empty? (rest coll))
+     (first coll)
+     (my-last (rest coll)))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (max (first a-seq) (max-element (rest a-seq))))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (seq-max (first a-seq) (longest-sequence (rest a-seq))))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons
+        (first a-seq)
+        (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+    (empty? a-seq)
+      false
+    (= (first a-seq) elem)
+      true
+    :else
+      (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+      a-seq
+    (pred? (first a-seq))
+      (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+    :else
+      '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+      a-seq
+    (pred? (first a-seq))
+      (my-drop-while pred? (rest a-seq))
+    :else
+      a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and (empty? a-seq) (empty? b-seq))
+      true
+    (or (empty? a-seq) (empty? b-seq))
+      false
+    (= (first a-seq) (first b-seq))
+      (seq= (rest a-seq) (rest b-seq))
+    :else
+      false))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
@@ -89,4 +139,3 @@
 
 (defn powerset [a-set]
   [:-])
-
