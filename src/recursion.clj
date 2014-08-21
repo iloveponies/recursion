37d7100affb5c5a8ee05ad5b06ebad4d604fcb5e
Teemu Tuulari <teemu.tuulari@greyarealabs.com>
2014-08-21 18:28:28 +0300

    Some funcs

--- src/recursion.clj
+++ src/recursion.clj
@@ -84,13 +84,24 @@
       false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+    '()
+    (cons (f (first seq-1) (first seq-2))
+          (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (= n 0)
+      0
+    (= n 1)
+      1
+    :else
+      (+ (fib (dec n)) (fib (dec (dec n))))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
