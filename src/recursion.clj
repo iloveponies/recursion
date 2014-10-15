60f17ca7629fd75d2fe72ac384009e13c3ea14d1
Martin <martin.b.radev@gmail.com>
2014-10-16 02:59:21 +0300

    almost done

--- src/recursion.clj
+++ src/recursion.clj
@@ -43,22 +43,25 @@
   )
 
 (defn seq-max [seq-1 seq-2]
+(if (> (count seq-1) (count seq-2)) seq-1 seq-2)
+  )
 
-  (if (< (count seq-1) (count seq-2))
 
-    seq-2
 
-    seq-1
+(defn longest-sequence [a-seq]
+
+  (if (empty? a-seq)
+
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (seq-max (first a-seq) (longest-sequence (rest a-seq)))
+      )
 
     )
 
   )
 
-(seq-max [1] [1 2])   ;=> [1 2]
-(seq-max [1 2] [3 4]) ;=> [3 4]
-
-(defn longest-sequence [a-seq]
-  [:-])
 
 (defn my-filter [pred? a-seq]
 
@@ -202,31 +205,9 @@
 
 
 (defn rotations [a-seq]
-
-  (let
-
-    [ini (rest ( reverse (inits a-seq)))
-     tail (rest (tails a-seq))
-     cnt ( dec (count a-seq))
-     arr (range 0 cnt)
-
-     ]
-
-
-    ((fn ! [subini subtail ]
-
-       (if (empty? subini)
-         '()
-         (cons (concat (first subtail) ( first subini)) ( ! (rest subini) (rest subtail)))
-         )
-
-
-       ) ini tail )
-
-
-    )
-
-  )
+(if (empty? a-seq)
+(cons [] [])
+(rest (map concat (tails a-seq) (reverse (inits a-seq))))))
 
 
 (defn my-frequencies-helper [freqs a-seq]
@@ -254,8 +235,6 @@
  )
 
 
-(my-frequencies [:a "moi" :a "moi" "moi" :a 1]) ;=> {:a 3, "moi" 3, 1 1}
-
 (defn un-frequencies [a-map]
 
   (apply concat (
@@ -269,10 +248,6 @@
    ))
 
   )
-
-(un-frequencies {:a 3 :b 2 "^_^" 1})             ;=> (:a :a :a "^_^" :b :b)
-(un-frequencies (my-frequencies [:a :b :c :a]))  ;=> (:a :a :b :c)
-(my-frequencies (un-frequencies {:a 100 :b 10})) ;=> {:a 100 :b 10}
 
 (defn my-take [n coll]
 
@@ -405,32 +380,26 @@
      v (into [] a-set)
      n (count a-set)
      ]
-    ((fn gen [after mp used]
+    ((fn gen [after mp used ]
 
        (if (>= after n)
-         (cons (calc a-set (vals mp)) '())
-         (let [q (map (fn [i]
+         (cons (calc a-set (vals mp) )'())
+         (let [q
+
+               (apply concat (map (fn [i]
 
                 (if (not (contains? used i))
 
+                    (gen (inc after) (assoc mp after i) (assoc used i 1) )
 
-                  (first (
-                   filter
-                   (complement empty?)
-
-                   (concat (gen (inc after) (assoc mp after i) (assoc used i 1) ) '())
-
-                   ))
-
-                  '()
 
                   )
 
                 ) (range 0 n)
-              )
+              ))
              ]
 
-          q
+            q
 
          )
          )
@@ -444,6 +413,33 @@
   )
 
 
+
+
+(defn comb [a-set n k ]
+
+  ((fn h [i after mp]
+    (map
+
+     (fn inner [idx]
+       (if (== i k)
+         (concat (calc a-set (vals (assoc mp (dec after) idx))) '())
+         (h (inc i) idx (assoc mp (dec after) idx))
+         )
+       )
+
+     (range (inc after) (inc n)))
+    ) 1 0 {})
+
+  )
+
+
 (defn powerset [a-set]
-  [:-])
+
+
+  (map (fn [k]
+         (comb a-set (count a-set) k)
+         ) (range 0 (+ 1 (count a-set))))
+
+
+  )
 
