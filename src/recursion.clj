2011bd5e18d710a7cd30261c374aab9710024cd3
s0rbus <soorbus@gmail.com>
2014-10-28 19:12:58 +0000

    complete to ex13

--- src/recursion.clj
+++ src/recursion.clj
@@ -36,22 +36,55 @@
         (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+    (empty? a-seq)
+	  false
+	(= elem (first a-seq))
+	  true
+	:else
+	  (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+       a-seq
+	(pred? (first a-seq))
+  	  (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+	:else
+	   []))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+       a-seq
+	(pred? (first a-seq))
+      (my-drop-while pred? (rest a-seq))
+	:else
+	   a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+     (or (and (empty? a-seq) (not (empty? b-seq)))
+         (and (not (empty? a-seq)) (empty? b-seq)))
+        false
+     (and (empty? a-seq) (empty? b-seq))
+       true
+     (= (first a-seq) (first b-seq))
+        (seq= (rest a-seq) (rest b-seq))
+     :else
+        false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (cond
+     (or (empty? seq-1) (empty? seq-2))
+	    ()
+	 :else
+	 (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+     1
+     (* n (power n (dec k)))))
 
 (defn fib [n]
   :-)
