25b001c116793c81aa88221cfa54213a9f64aa26
samutamm <samutamm@gmail.com>
2014-12-24 19:35:04 +0200

    Exercises 1-7.

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,22 +1,36 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (cond
+   (empty? coll) false
+   (not (empty? (rest coll))) false
+   :else true))
 
 (defn my-last [coll]
-  :-)
+  (if (or (empty? coll) (singleton? coll))
+    (first coll)
+    (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (or (empty? a-seq) (singleton? a-seq))
+    (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? (rest a-seq))
+    (first a-seq)
+    (seq-max (first a-seq)(longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
   [:-])
