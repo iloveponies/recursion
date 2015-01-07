8f10b8f1daaaeebcfddae482c55c574ae3e9b70a
yerejm <git@thelostyerejm.com>
2015-01-07 22:25:50 +1000

    recursion tests up to encore passed

--- src/recursion.clj
+++ src/recursion.clj
@@ -122,31 +122,66 @@
           (inits (reverse (rest (reverse a-seq)))))))
 
 (defn rotations [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    [a-seq]
+    (let [a (rest (tails a-seq))
+          b (rest (reverse (inits a-seq)))]
+      (map concat a b))))
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (my-frequencies-helper
+      (assoc freqs
+             (first a-seq)
+             (+ 1 (let [val (get freqs (first a-seq))]
+                    (if (nil? val)
+                      0
+                      val))))
+      (rest a-seq))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (apply concat (map (fn [x] (let [[k n] x] (repeat n k))) a-map)))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (empty? coll) (= 0 n))
+    '()
+    (cons (first coll) (my-take (- n 1) (rest coll)))))
 
+(defn my-drop-helper [m n coll]
+  (cond
+    (empty? coll) '()
+    (< m n) (my-drop-helper (+ m 1) n (rest coll))
+    :else (cons (first coll) (my-drop-helper m n (rest coll)))))
 (defn my-drop [n coll]
-  [:-])
+    (my-drop-helper 0 n coll))
 
 (defn halve [a-seq]
-  [:-])
+  (let [mid-pt (int (/ (count a-seq) 2))]
+    (vector
+      (my-take mid-pt a-seq)
+      (my-drop mid-pt a-seq))))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+   (cond
+     (and (empty? a-seq) (empty? b-seq)) '()
+     (empty? a-seq) (cons (first b-seq) (seq-merge a-seq (rest b-seq)))
+     (empty? b-seq) (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+     (< (first b-seq) (first a-seq)) (cons (first b-seq) (seq-merge a-seq (rest b-seq)))
+     (< (first a-seq) (first b-seq)) (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+     :else (cons (first a-seq) (cons (first b-seq) (seq-merge (rest a-seq) (rest b-seq))))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    (empty? (rest a-seq)) a-seq
+    :else (let [[s1 s2] (halve a-seq)]
+            (seq-merge
+              (merge-sort s1)
+              (merge-sort s2)))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
