533f4cd20915fa88e9a700fc66f72fc9fef046b0
jykke <jyri.lehva@saunalahti.fi>
2015-02-03 00:04:50 +0200

    exercise 8

--- src/recursion.clj
+++ src/recursion.clj
@@ -35,7 +35,11 @@
     :else (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    :else (cond (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      :else (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
   :-)
