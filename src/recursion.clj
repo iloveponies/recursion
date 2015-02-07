e840750bf66fd087c481dcf8ae9f646a80586fd8
jykke <jyri.lehva@saunalahti.fi>
2015-02-07 16:19:34 +0200

    exercise 12 seq=

--- src/recursion.clj
+++ src/recursion.clj
@@ -66,7 +66,15 @@
       a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and (empty? a-seq) (empty? b-seq))
+      true
+    (or (empty? a-seq) (empty? b-seq))
+      false
+    (= (first a-seq) (first b-seq))
+      (seq= (rest a-seq) (rest b-seq))
+    :else
+      false))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
