1f6a146338fc148ad60dfb1e2b062c74ddddc3b6
jykke <jyri.lehva@saunalahti.fi>
2015-02-08 16:22:25 +0200

    exercise 16 my-repeat

--- src/recursion.clj
+++ src/recursion.clj
@@ -98,7 +98,9 @@
       (+ (fib (dec n)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (< how-many-times 1)
+    ()
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
   [:-])
