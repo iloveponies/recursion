1e3bd49a95412934ba51deddd4f87294ca32896f
Tuomo Niemi <tuomo.t.niemi@luukku.com>
2015-03-05 21:31:23 +0200

    Mergesorts done

--- src/recursion.clj
+++ src/recursion.clj
@@ -186,10 +186,23 @@
     [(my-take mid a-seq) (my-drop mid a-seq)]))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond
+   (and (empty? a-seq) (empty? b-seq))
+     '()
+   (empty? a-seq)
+     b-seq
+   (empty? b-seq)
+     a-seq
+   (< (first a-seq) (first b-seq))
+     (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+   :else
+     (cons (first b-seq) (seq-merge a-seq (rest b-seq)))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (or (empty? a-seq) (== (count a-seq) 1))
+    a-seq
+    (let [[alku loppu] (halve a-seq)]
+      (seq-merge (merge-sort alku) (merge-sort loppu)))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
