205e685543009f2a653a2b7eed82648fdb0f612f
Md. Mesbahul Islam <mdislam@cs.helsinki.fi>
2015-03-09 16:43:38 +0200

    completed 11 exercises

--- src/recursion.clj
+++ src/recursion.clj
@@ -17,31 +17,47 @@
       (my-last (rest coll)))))
 
 (defn max-element [a-seq]
-  (if (empty? a-seq)
-    nil
-    (let [cur (first a-seq)
-          nxt (max-element (rest a-seq))]
-      (if (= nxt nil)
-        cur
-        (max cur nxt)))))
+  (cond (empty? a-seq) nil
+        (singleton? a-seq) (first a-seq)
+        :else (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2)) seq-1 seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq) nil
+    (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq) a-seq
+    (if (pred? (first a-seq))
+      (cons (first a-seq) (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond (empty? a-seq) false
+        (= elem (first a-seq)) true
+        :else
+        (sequence-contains? elem (rest a-seq))
+        ))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)
+     a-seq
+   (pred? (first a-seq))
+     (cons (first a-seq)(my-take-while pred? (rest a-seq)))
+   :else
+     []))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)
+     a-seq
+   (pred? (first a-seq))
+     (my-drop-while pred? (rest a-seq))
+   :else
+     a-seq))
 
 (defn seq= [a-seq b-seq]
   :-)
