d23bdadcf5cdbe4552f4a0242e3ccc79f7ac721a
Alexander Moeser <ajmoeser@gmail.com>
2015-05-24 18:07:18 -0700

    added or-empty? line to seq=

--- src/recursion.clj
+++ src/recursion.clj
@@ -60,6 +60,7 @@
 (defn seq= [a-seq b-seq]
   (cond
     (and (empty? a-seq) (empty? b-seq)) true
+    (or (empty? a-seq) (empty? b-seq)) false
     (and (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))) true
     :else false))
 
