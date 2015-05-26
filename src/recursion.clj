b08d3a2c21bf00712c4b561b636801dfb4e44ef9
ybykov <yury.bykov@gmail.com>
2015-05-26 17:21:47 +0200

    in progress

--- src/recursion.clj
+++ src/recursion.clj
@@ -43,9 +43,9 @@
           (if (empty? a-seq)
             acc
             (if (pred? (first a-seq))
-              (fltr pred? (rest a-seq) (cons (first a-seq) acc))
+              (fltr pred? (rest a-seq) (conj acc (first a-seq)))
               (fltr pred? (rest a-seq) acc))))]
-    (helper pred? a-seq ())))
+    (helper pred? a-seq [])))
 
 (defn sequence-contains? [elem a-seq]
   (cond
@@ -54,37 +54,65 @@
    :else true))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq)) (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+   :else ()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+   (empty? a-seq) a-seq
+   (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+   :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+   (and (empty? a-seq) (empty? b-seq)) true
+   (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))
+   :else false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (cond
+   (or (empty? seq-1) (empty? seq-2)) ()
+   :else (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+   (zero? n) 0
+   (= n 1) 1
+   :else (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (> how-many-times 0)
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))
+    ()))
 
 (defn my-range [up-to]
-  [:-])
+  (if (> up-to 0)
+    (cons (dec up-to) (my-range (dec up-to)))
+    ()))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '(())
+    (cons a-seq (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (map reverse (tails (reverse a-seq))))
+
 
 (defn rotations [a-seq]
-  [:-])
+  (let [helper
+        (fn rt [a-seq acc]
+          (if (empty? a-seq)
+            ()
+            (cons (concat a-seq acc) (rt (rest a-seq) (conj acc (first a-seq))))))]
+    (helper a-seq [])))
 
 (defn my-frequencies-helper [freqs a-seq]
   [:-])
