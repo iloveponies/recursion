9c9cf4c4cc0b61a2103e3f411ee01ae500bf00ec
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-07-10 00:13:23 +0300

    Functions product through max-element.

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,16 +1,27 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  ;(apply * coll)
+  (if (empty? coll)
+    1
+    (* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (not (empty? coll))
+       (empty? (rest coll))))
+
+(defn- empty-or-singleton? [coll]
+  (or (empty? coll) (singleton? coll)))
 
 (defn my-last [coll]
-  :-)
+  (if (empty-or-singleton? coll)
+    (first coll)
+    (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty-or-singleton? a-seq)
+    (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
   [:-])
