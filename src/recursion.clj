856c74191cc14527025ebd3e6dcb5b38dbe80dfc
Igor Garcia <igarciauk@gmail.com>
2015-11-04 09:03:31 +0000

    Solved Exercise 14

--- src/recursion.clj
+++ src/recursion.clj
@@ -80,7 +80,8 @@
    (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k) 1
+  (* n (power n (dec k)))))
 
 (defn fib [n]
   :-)
