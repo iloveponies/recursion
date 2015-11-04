7d6134694c38b72a94f579de1e1d20d6682c2e0b
Igor Garcia <igarciauk@gmail.com>
2015-11-04 09:40:39 +0000

    Solved Exercise 15

--- src/recursion.clj
+++ src/recursion.clj
@@ -84,7 +84,11 @@
   (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (zero? n) 0
+    (= 1 n) 1
+    :else
+    (+ (fib (dec n)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
