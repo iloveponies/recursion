6fb1f9ac2020b9c84f8730ca1193f67524042688
mimir0n <n-bar>
2015-11-08 10:48:10 +0300

    1-26

--- src/recursion.clj
+++ src/recursion.clj
@@ -8,9 +8,8 @@
 
 (defn singleton? [coll]
 ;;   helper
-  (and
-   (boolean (first coll))
-   (empty? (rest coll))))
+  (and (not (empty? coll))
+       (empty? (rest coll))))
 
 (defn my-last [coll]
   (if (or (singleton? coll) (empty? coll))
@@ -66,9 +65,13 @@
 
 (defn seq= [a-seq b-seq]
   (cond
-   (and (empty? a-seq) (empty? b-seq)) true
+   (and (empty? a-seq) (not (empty? b-seq))) false
+   (and (not (empty? a-seq)) (empty? b-seq)) false
    (not= (first a-seq) (first b-seq)) false
+   (and (empty? a-seq) (empty? b-seq)) true
    :else (seq= (rest a-seq) (rest b-seq))))
+
+(and true false)
 
 (defn my-map [f seq-1 seq-2]
   (if (or (empty? seq-1) (empty? seq-2))
@@ -113,32 +116,57 @@
 
 (defn rotations [a-seq]
   (if (empty? a-seq)
-    '()
+    (list '())
     (rest (map concat (reverse (tails a-seq))  (inits a-seq)))))
 
+
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (let [fst (first a-seq)
+          new-freqs (assoc freqs fst (inc (freqs fst 0)))]
+      (my-frequencies-helper new-freqs (rest a-seq)))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (if (empty? a-map)
+    '()
+    (concat
+     (apply repeat (reverse (first a-map)))
+     (un-frequencies (rest a-map)))))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (zero? n) (empty? coll))
+    '()
+    (cons (first coll) (my-take (dec n) (rest coll)))))
 
 (defn my-drop [n coll]
-  [:-])
+  (if (or (zero? n) (empty? coll))
+    coll
+    (my-drop (dec n) (rest coll))))
 
 (defn halve [a-seq]
-  [:-])
+  [(my-take (quot (count a-seq) 2) a-seq)
+   (my-drop (quot (count a-seq) 2) a-seq)])
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (let [fst (first a-seq)
+        lt-fst? (fn [x] (< x fst))]
+    (if (empty? a-seq)
+      b-seq
+      (seq-merge (rest a-seq)
+                 (concat (my-take-while lt-fst? b-seq)
+                         (list fst)
+                         (my-drop-while lt-fst? b-seq))))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (or (singleton? a-seq) (empty? a-seq))
+    a-seq
+    (let [[part-1 part-2] (halve a-seq)]
+      (seq-merge (merge-sort part-1)
+                 (merge-sort part-2)))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
@@ -148,4 +176,3 @@
 
 (defn powerset [a-set]
   [:-])
-
