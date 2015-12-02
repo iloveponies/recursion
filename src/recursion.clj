10d4ac073b472172c10d3c1049293e05de8f51e1
Jonathan Headen <jonathan.headen@outlook.com>
2015-12-02 17:22:21 +1100

    fixxed issue with inits

--- src/recursion.clj
+++ src/recursion.clj
@@ -106,12 +106,12 @@
 
 (defn tails [a-seq]
   (if (empty? a-seq)
-    (cons a-seq nil)
+    (cons a-seq '())
     (cons a-seq (tails (rest a-seq)))
     ))
 
 (defn inits [a-seq]
-  (reverse (tails (reverse a-seq))))
+  (map reverse (tails (reverse a-seq))))
 
 (defn rotations [a-seq]
   (let [double-seq (concat a-seq a-seq)
