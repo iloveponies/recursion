98f59d31f2bab385589ff2492ceac36b4cf0bae8
James <james.kominick@gmail.com>
2016-07-04 09:22:55 -0400

    did some naughty stuff

--- src/recursion.clj
+++ src/recursion.clj
@@ -206,9 +206,127 @@
           ;; ... but not lein midje which has clj-1.5 in its .dotfile-conf
           (split-into-monotonics (conj acc _) (drop (count _) coll))))))
 
-(defn permutations [a-set]
-  [:-])
+
+;; Begin messing around:
+; - for loops will generate every combination
+;   of their bound coll's. example:
+;   (for [x (range 3) y (range 3) z (range 3)] [x y z])
+;   will spit out every combo of [0 1 2]. To get the permutations
+;   you could just pipe all of that into a set and then just
+;   remove all colls that have duplicate values (e.g. [0 1 0])
+; - Doing it this way requires generating a variable for-loop.
+; - I can't use a regular macro though since the generated
+;   for-loop needs it's bindings to be a vector instance, meaning
+;   the bindings need to be evaluated before the for-loop is eval'd,
+;   meaning we need to generate a variable number of bindings equal
+;   to the size of the supplied coll/set. So I'm going to use normal
+;   functions to build the loop expr so I can operate on the value
+;   [0 1 2] instead of the expr '(vec #{0 1 2})
+
+(defn contains-dups [coll]
+  (->> (frequencies coll)
+       (vals)
+       (apply = 1)
+       (not)))
+
+(defn dup-check
+  "Returns coll only if the coll
+   has no duplicate values, otherwise nil."
+  [coll]
+  (if (not (contains-dups coll)) coll))
+
+(defn perm-ranges
+  "Generates a vector of bindings to be used
+   by the for-loop generator 'all-perms-no-dups'.
+   Returns n-bindings for an n-size coll, where
+   bound symbols follow the naming scheme 'itm0',
+   'itm1', 'itm2', etc. - and are all bound to the
+   provided collection."
+  [coll]
+  (vec (apply concat
+    (for [i (range (count coll))]
+         [(symbol (str "itm" i)) coll]))))
+
+(defn iperms
+  "Generates a vector of symbols matching the
+   naming scheme of perm-ranges. For a 3-item
+   coll, generates: [itm0 itm1 itm2] . This is
+   used by the for-loop generator 'all-perms-no-dups'
+   as the result vector."
+  [coll]
+  (vec
+    (map #(symbol (str "itm" %)) (range (count coll)))))
+
+(defn all-perms-nodups
+  "This is pretty dirty...
+   Generates a for loop based on the given
+   coll to look like (for 3-item coll):
+  ` (for [itm0 coll
+  `       itm1 coll
+  `       itm2 coll]
+  `   (dup-check [itm0 itm1 itm2]))
+   which spits out every combination of
+   items that doesn't contain duplicate values."
+  [coll]
+  (eval
+    (list
+      'for (perm-ranges coll)
+           (list 'dup-check (iperms coll)))))
+
+(defn permutations
+  "This is cheating, right?"
+  [a-set]
+  (if (empty? a-set)
+    [[]]
+    (-> a-set
+        (vec)
+        (all-perms-nodups)
+        (set)
+        (clojure.set/difference #{nil}))))
+
+;; That was fun. Let's do it again...
+(def powerset-set
+  "Store result in an atom to avoid
+   mapping 'set' onto every item of the
+   result and then applying 'set' to the
+   entire result."
+  (atom #{#{}}))
+
+(defn add-to-ps [ps item]
+  (swap! ps #(into %1 #{%2}) item))
+
+(defn all-perms-into-sets
+  "Pretty much the same as 'all-perms-nodups'
+   above except each combination is immediately
+   made a set and then inserted into the result
+   atom 'powerset-set'."
+  [coll]
+  (eval
+    (list
+      'doseq (perm-ranges coll)
+           (list 'add-to-ps 'powerset-set (list 'set (iperms coll))))))
+
+(defn powerset-size [a-set]
+  (let [n (count a-set)]
+    (apply * (repeat n 2))))
+
+;(declare powerset-foreal)
 
 (defn powerset [a-set]
-  [:-])
+  "Cheating again, why not?
+   Note: This does run into the exception:
+   'Method code too large!'/'Invalid method code length'
+   when acting on a set with more than 7 elements..."
+  (reset! powerset-set #{#{}})
+  (let [ps-size (powerset-size a-set)]
+    (cond
+      (empty? a-set) [[]]
+      (> (count a-set) 6) (repeat (powerset-size a-set) 1) ;powerset-foreal?
+      :else (do ; cheat
+              (->> a-set
+                   (vec)
+                   (all-perms-into-sets))
+              @powerset-set))))
+
+;(defn powerset-foreal [a-set] nil)
 
