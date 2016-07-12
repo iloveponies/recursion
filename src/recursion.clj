b549116b1415ba1b463144ae8759652cf48b4599
Maxim Litvinov <maxim.litvinov@slingmedia.com>
2016-07-12 16:29:49 -0700

    Exercise 22 - 25

--- src/recursion.clj
+++ src/recursion.clj
@@ -168,25 +168,67 @@
     (rotations-r [] (vec a-seq) a-seq)))
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (let [a-item (first a-seq)
+          a-seq-next (next a-seq)
+          item-freq (get freqs a-item 0)
+          freqs-next (assoc freqs a-item (inc item-freq))]
+      (recur freqs-next a-seq-next))))
+
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
+
+(defn un-frequencies-recur [a-seq a-map]
+  (if (empty? a-map)
+    a-seq
+    (let [a-map-item (first a-map)
+          a-map-next (next a-map)
+          item (key a-map-item)
+          item-count (val a-map-item)
+          item-repeated (repeat item-count item)
+          a-seq-next (concat a-seq item-repeated)]
+      (recur a-seq-next a-map-next))))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (un-frequencies-recur `() a-map))
+
+(defn my-split-recur [vec-before non-zero a-seq-after]
+  (if (or (zero? non-zero) (empty? a-seq-after))
+    (vector (or (seq vec-before) `()) a-seq-after)
+    (let [a-item (first a-seq-after)
+          a-seq-after-next (next a-seq-after)
+          a-vec-before-next (conj vec-before a-item)]
+      (recur a-vec-before-next (dec non-zero) a-seq-after-next))))
 
 (defn my-take [n coll]
-  [:-])
+  (get (my-split-recur [] n coll) 0))
+
+(defn my-drop-recur [non-zero a-seq]
+  (if (or (zero? non-zero) (empty? a-seq))
+    (or a-seq `())
+    (recur (dec non-zero) (next a-seq))))
 
 (defn my-drop [n coll]
-  [:-])
+  (my-drop-recur n coll))
 
 (defn halve [a-seq]
-  [:-])
+  (let [a-count (count a-seq)
+        middle (int (/ a-count 2))]
+   (my-split-recur [] middle a-seq)))
+
+(defn seq-merge-recur [a-vec-res a-seq b-seq]
+  (let [a-item (first a-seq)
+        b-item (first b-seq)]
+    (cond
+      (empty? a-seq) (concat a-vec-res b-seq)
+      (empty? b-seq) (concat a-vec-res a-seq)
+      (<= a-item b-item) (recur (conj a-vec-res a-item) (next a-seq) b-seq)
+      :else (recur (conj a-vec-res b-item) a-seq (next b-seq)))))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (seq-merge-recur [] a-seq b-seq))
 
 (defn merge-sort [a-seq]
   [:-])
