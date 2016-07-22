ee148c9a66f27d23904127befcd67463b8094686
ahri <ahri.spam@gmail.com>
2016-07-22 17:24:21 +0200

    recursion part 1

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,92 +1,115 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+	(if (empty? coll)
+		1
+		(* (first coll) (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+	(and (not (empty? coll))
+		 (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+	(cond
+		(empty? coll) nil
+		(singleton? coll) (first coll)
+		:else (my-last (rest coll))))
+
 
 (defn max-element [a-seq]
-  :-)
+	(let [a (first a-seq)]
+		(cond
+			(empty? a-seq) nil
+			(singleton? a-seq) a
+			:else (max a (max-element (rest a-seq))))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+	(if (> (count seq-1) (count seq-2))
+		seq-1
+		seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+	(let [a (first a-seq)]
+		(cond
+			(empty? a-seq) nil
+			(singleton? a-seq) a
+			:else (seq-max a (longest-sequence (rest a-seq))))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+	(if (empty? a-seq)
+		a-seq
+		(let [a (first a-seq)
+			  r (my-filter pred? (rest a-seq))]
+			(if (pred? a)
+				(cons a r)
+				r))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+	:-)
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+	[:-])
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+	[:-])
 
 (defn seq= [a-seq b-seq]
-  :-)
+	:-)
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+	[:-])
 
 (defn power [n k]
-  :-)
+	:-)
 
 (defn fib [n]
-  :-)
+	:-)
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+	[:-])
 
 (defn my-range [up-to]
-  [:-])
+	[:-])
 
 (defn tails [a-seq]
-  [:-])
+	[:-])
 
 (defn inits [a-seq]
-  [:-])
+	[:-])
 
 (defn rotations [a-seq]
-  [:-])
+	[:-])
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+	[:-])
 
 (defn my-frequencies [a-seq]
-  [:-])
+	[:-])
 
 (defn un-frequencies [a-map]
-  [:-])
+	[:-])
 
 (defn my-take [n coll]
-  [:-])
+	[:-])
 
 (defn my-drop [n coll]
-  [:-])
+	[:-])
 
 (defn halve [a-seq]
-  [:-])
+	[:-])
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+	[:-])
 
 (defn merge-sort [a-seq]
-  [:-])
+	[:-])
 
 (defn split-into-monotonics [a-seq]
-  [:-])
+	[:-])
 
 (defn permutations [a-set]
-  [:-])
+	[:-])
 
 (defn powerset [a-set]
-  [:-])
+	[:-])
 
