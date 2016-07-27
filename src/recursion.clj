0a0db7b335b1be7d59f888bca0b59fdeaec5d8a8
Juho Leinonen <juholei@gmail.com>
2016-07-27 19:02:35 +0300

    18 exercises done

--- src/recursion.clj
+++ src/recursion.clj
@@ -94,10 +94,14 @@
     (cons (dec up-to) (my-range (dec up-to)))))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq) 
+    '(())
+    (cons a-seq (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '(())
+    (cons a-seq (inits (reverse (rest (reverse a-seq)))))))
 
 (defn rotations [a-seq]
   [:-])
