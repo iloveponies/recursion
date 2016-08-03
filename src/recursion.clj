f53696cacbbffbd489d5791aa0b0a3811a6bf4ba
Juho Leinonen <juholei@gmail.com>
2016-08-03 19:52:13 +0300

    All normal exercises done

--- src/recursion.clj
+++ src/recursion.clj
@@ -136,10 +136,16 @@
     [(my-take half-count a-seq) (my-drop half-count a-seq)]))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond
+    (empty? a-seq) b-seq
+    (empty? b-seq) a-seq
+    (< (first a-seq) (first b-seq)) (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+    :else (cons (first b-seq) (seq-merge a-seq (rest b-seq)))))
 
 (defn merge-sort [a-seq]
-  [:-])
+  (cond
+    (or (zero? (count a-seq)) (= 1 (count a-seq))) a-seq
+    :else (seq-merge (merge-sort (first (halve a-seq))) (merge-sort (second (halve a-seq))))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
