0e272960477bb9c1dd6acfcc6490cd21b1a9e048
Jake Hendrickson <jake.hendrickson@gmail.com>
2016-08-16 14:18:02 +0000

    Excercises done. One test fails b/c powerset returns empty list instead of empty set in null case

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject recursion "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/recursion "0.1.0-SNAPSHOT"]]
+                 [iloveponies.tests/recursion "0.1.0-SNAPSHOT"]
+                 [org.clojure/tools.trace "0.7.9"]
+                 ]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
