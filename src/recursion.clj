bf41e1c0c6c0aada2248237720ab138be9ed1a69
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-10-20 22:18:05 +0300

    Solve seq-merge

--- src/recursion.clj
+++ src/recursion.clj
@@ -143,7 +143,11 @@
     (list (drop half a-seq)))))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond
+    (empty? a-seq) b-seq
+    (empty? b-seq) a-seq
+    (< (first a-seq) (first b-seq)) (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+    :else (cons (first b-seq) (seq-merge a-seq (rest b-seq)))))
 
 (defn merge-sort [a-seq]
   [:-])
