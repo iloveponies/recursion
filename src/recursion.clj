d110a79b643982ddd9ef3cb81e2bc4adcceaef2d
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-10-20 21:06:34 +0300

    Solve seq=

--- src/recursion.clj
+++ src/recursion.clj
@@ -62,7 +62,12 @@
     a-seq)))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and (empty? a-seq) (empty? b-seq)) true
+    (empty? a-seq) false
+    (empty? b-seq) false
+    (not (= (first a-seq) (first b-seq))) false
+    :else (seq= (rest a-seq) (rest b-seq))))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
