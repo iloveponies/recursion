fab20d203c7f66bfa872102aaf002441d1087327
Heikki Holopainen <heikki.holopainen@gmail.com>
2016-12-28 23:47:32 +0200

    max-element & seq-max

--- src/recursion.clj
+++ src/recursion.clj
@@ -19,10 +19,20 @@
     (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+    (singleton? a-seq) (first a-seq)
+    (empty? a-seq) nil
+    :else
+    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if
+    (> (count seq-1)
+       (count seq-2))
+    seq-1
+    seq-2))
+
+
 
 (defn longest-sequence [a-seq]
   [:-])
