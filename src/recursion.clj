d9342247730c375b2226191e78b7deeb26cde9e4
Heikki Holopainen <heikki.holopainen@gmail.com>
2016-12-30 15:15:54 +0200

    sequence-contains?

--- src/recursion.clj
+++ src/recursion.clj
@@ -50,7 +50,13 @@
 
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+    (empty? a-seq)
+    false
+    (= elem (first a-seq))
+    true
+    :else
+    (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
   [:-])
