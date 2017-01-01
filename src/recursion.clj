7793ba51b239e748b98a7bf2971b37e6c99d440c
Heikki Holopainen <heikki.holopainen@gmail.com>
2017-01-01 21:16:45 +0200

    refactor fib

--- src/recursion.clj
+++ src/recursion.clj
@@ -117,10 +117,9 @@
       (power n (dec k)))))
 
 (defn fib [n]
-  (cond
-    (= n 0) 0
-    (= n 1) 1
-    :else
+  (if
+    (< n 2)
+    n
     (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
