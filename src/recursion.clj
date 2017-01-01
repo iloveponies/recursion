0e9547121f4c800667affd9f7469c996713bace4
Heikki Holopainen <heikki.holopainen@gmail.com>
2017-01-01 20:49:48 +0200

    seq=

--- src/recursion.clj
+++ src/recursion.clj
@@ -80,7 +80,24 @@
 
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and
+      (empty? a-seq)
+      (empty? b-seq))
+    true
+    (or
+      (empty? a-seq)
+      (empty? b-seq))
+    false
+    :else
+    (cond
+      (=
+      (first a-seq)
+      (first b-seq))
+      (recur (rest a-seq) (rest b-seq))
+      :else
+      false)))
+
 
 (defn my-map [f seq-1 seq-2]
   [:-])
