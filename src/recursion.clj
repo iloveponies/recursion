141748e6549bf0d9f7fd03237cca564e43c8dde8
Santeri Moilanen <h9029@student.jamk.fi>
2017-02-02 18:09:16 +0200

    jee pari eteenpäi

--- src/recursion.clj
+++ src/recursion.clj
@@ -72,13 +72,24 @@
    :else false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (cond
+  (or (empty? seq-2) (empty? seq-1)) '()
+  :else
+  (cons (f (first seq-1) (first seq-2))
+  (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k) 1
+    (* n (power n (dec k)))
+  )
+)
 
 (defn fib [n]
-  :-)
+  (cond
+   (zero? n) 0
+   (== n 1) 1
+   :else
+   (+ (fib (- n 1)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
