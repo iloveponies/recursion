7338890d2c074b94f79fb848c376a7bb7be06d2d
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-03-28 20:00:03 +0300

    my-map done

--- src/recursion.clj
+++ src/recursion.clj
@@ -56,10 +56,15 @@
       :else a-seq)))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond (and (empty? a-seq) (empty? b-seq)) true
+        (or (empty? a-seq) (empty? b-seq)) false
+        (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))
+        :else false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+    []
+    (cons (f (first seq-1) (first seq-2)) (my-map f (rest seq-1) (rest seq-2)))))
 
 (defn power [n k]
   :-)
