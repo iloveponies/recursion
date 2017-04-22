c24a73a0b0e2e6864176594212533f8bf70997ba
Tuomas Lappeteläinen <tuomas.lappetelainen@gmail.com>
2017-04-22 21:22:32 +0300

    finish until rotations

--- src/recursion.clj
+++ src/recursion.clj
@@ -9,56 +9,104 @@
 (defn singleton? [coll]
   (and (not (empty? coll)) (empty? (rest coll))))
 
+(defn one-or-less? [coll]
+  (or (empty? coll) (empty? (rest coll))))
+
 (defn my-last [coll]
   (nth coll (dec (count coll)) nil))
 
 (defn max-element [a-seq]
-  (cond
-    (empty? a-seq) nil
-    (singleton? a-seq) (first a-seq)
-    :else (max (first a-seq) (max-element (rest a-seq)))))
+  (if (one-or-less? a-seq)
+    (first a-seq)
+    (max (first a-seq) (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (one-or-less? a-seq)
+    (first a-seq)
+    (seq-max (first a-seq) (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) '()
+    (pred? (first a-seq)) (cons (first a-seq)
+                                (my-filter pred? (rest a-seq)))
+    :else (my-filter pred? (rest a-seq))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+    (empty? a-seq) false
+    (= elem (first a-seq)) true
+    :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (if (and
+       (not (empty? a-seq))
+       (pred? (first a-seq)))
+    (cons (first a-seq)
+          (my-take-while pred? (rest a-seq)))
+    '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (if (and
+       (not (empty? a-seq))
+       (pred? (first a-seq)))
+    (my-drop-while pred? (rest a-seq))
+    a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and (empty? a-seq) (empty? b-seq)) true
+    ;; (= (first [nil]) (first [])) is of course true...
+    (or (empty? a-seq) (empty? b-seq)) false
+    (= (first a-seq) (first b-seq)) (seq= (rest a-seq) (rest b-seq))
+    :else false))
 
-(defn my-map [f seq-1 seq-2]
-  [:-])
+
+(defn my-map [f a-seq b-seq]
+  (if (or (empty? a-seq) (empty? b-seq))
+    '()
+    (cons
+     (f (first a-seq) (first b-seq))
+     (my-map f (rest a-seq) (rest b-seq)))))
 
 (defn power [n k]
-  :-)
+  (if (== k 0)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (== n 0) 0
+    (== n 1) 1
+    :else (+ (fib (dec n)) (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (<= how-many-times 0)
+    '()
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (if (<= up-to 0)
+    '()
+    (cons (dec up-to) (my-range (dec up-to)))))
+
+(defn tails-rec [a-seq]
+  (if (empty? a-seq)
+    '()
+    (cons a-seq (tails-rec (rest a-seq)))))
 
 (defn tails [a-seq]
-  [:-])
+  ;; How to append '() with cons..
+  (conj (tails-rec a-seq) '()))
 
 (defn inits [a-seq]
-  [:-])
+  ;; Not quite sure about this..
+  (map reverse (tails (reverse a-seq))))
 
 (defn rotations [a-seq]
   [:-])
