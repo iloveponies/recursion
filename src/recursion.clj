de2c61f20c1130a3ee47f3444a202550ffba8556
Ilmo Raunio <ilmo.raunio@solita.fi>
2017-04-27 13:17:17 +0300

    Some answers

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,25 +1,37 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (reduce (fn [a b] (* a b)) 1 coll))
 
 (defn singleton? [coll]
-  :-)
+  (= 1 (reduce (fn [a b] (inc a)) 0 coll)))
 
 (defn my-last [coll]
-  :-)
+  (loop [[val & rest] coll]
+    (if rest
+      (recur rest)
+      val)))
 
 (defn max-element [a-seq]
-  :-)
+  (loop [[val & rest] a-seq
+         highest val]
+    (if rest
+      (if (> val highest)
+        (recur rest val)
+        (recur rest highest))
+      highest)))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (first (sort-by count #(>= %1 %2) [seq-1 seq-2])))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (loop [[val & rest] a-seq
+         longest val]
+    (if rest
+      (recur rest (seq-max val longest))
+      longest)))
 
-(defn my-filter [pred? a-seq]
-  [:-])
+(defn my-filter [pred? a-seq])
 
 (defn sequence-contains? [elem a-seq]
   :-)
