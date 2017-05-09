796082afb6617a285ef2730dddf9a1e2ab97791f
Toni Räsänen <toni.rasanen@iki.fi>
2017-05-09 17:48:21 +0300

    Exercise 12

--- src/recursion.clj
+++ src/recursion.clj
@@ -86,62 +86,86 @@
         (cons first-elem rest-of-elems)))))
 
 
-
 (defn seq= [a-seq b-seq]
-  :-)
+  "Exercise 12"
+  (cond
+    (not (= (count a-seq) (count b-seq))) false
+    (and (empty? a-seq) (empty? b-seq)) true
+    (= (first a-seq) (first b-seq))
+    (seq= (rest a-seq) (rest b-seq))
+    :else false))
+
 
 (defn my-map [f seq-1 seq-2]
+  "Exercise 13"
   [:-])
 
 (defn power [n k]
+  "Exercise 14"
   :-)
 
 (defn fib [n]
+  "Exercise 14"
   :-)
 
 (defn my-repeat [how-many-times what-to-repeat]
+  "Exercise 15"
   [:-])
 
 (defn my-range [up-to]
+  "Exercise 16"
   [:-])
 
 (defn tails [a-seq]
+  "Exercise 17"
   [:-])
 
 (defn inits [a-seq]
+  "Exercise 18"
   [:-])
 
 (defn rotations [a-seq]
+  "Exercise 19"
   [:-])
 
 (defn my-frequencies-helper [freqs a-seq]
+  "Exercise 20"
   [:-])
 
 (defn my-frequencies [a-seq]
+  "Exercise 21"
   [:-])
 
 (defn un-frequencies [a-map]
+  "Exercise 22"
   [:-])
 
 (defn my-take [n coll]
+  "Exercise 23"
   [:-])
 
 (defn my-drop [n coll]
+  "Exercise 24"
   [:-])
 
 (defn halve [a-seq]
+  "Exercise 25"
   [:-])
 
 (defn seq-merge [a-seq b-seq]
+  "Exercise 26 "
   [:-])
 
 (defn merge-sort [a-seq]
+  "Exercise 27"
   [:-])
 
 (defn split-into-monotonics [a-seq]
+  "Exercise 28"
   [:-])
 
 (defn permutations [a-set]
+  "Exercise 29"
   [:-])
 
 (defn powerset [a-set]
