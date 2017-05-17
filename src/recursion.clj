90440f07d1b9e99fc124ae02ed5ab00481e7cd3b
Topi T <smobey@gmail.com>
2017-05-17 18:45:33 +0300

    bare minimum for now

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,16 +1,32 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+      (* (first coll)
+        (product (rest coll))))
+)
 
 (defn singleton? [coll]
-  :-)
+  (cond
+    (empty? coll) false
+    (empty? (rest coll)) true
+    :else false)
+)
 
 (defn my-last [coll]
-  :-)
+  (if (empty? (rest coll))
+    (first coll)
+    (my-last (rest coll)))
+)
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (max (first a-seq) (max-element (rest a-seq)))))
+)
 
 (defn seq-max [seq-1 seq-2]
   [:-])
