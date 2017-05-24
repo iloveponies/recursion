5caba5b9dc963a0a2bb61e4932b6f5779d92b921
Ilmo Raunio <ilmo.raunio@solita.fi>
2017-05-24 12:57:16 +0300

    Small refactoring of encore answers

--- src/recursion.clj
+++ src/recursion.clj
@@ -217,7 +217,7 @@
   (if (seq (rest a-set))
     (apply concat (for [x a-set]
                     (map #(cons x %) (permutations (remove #{x} a-set)))))
-    [a-set])
+    (list a-set))
 
   ;; zero points for cheating :)
   [:-])
@@ -228,4 +228,4 @@
            (for [x a-set]
              (concat [#{}] [#{x}]
                      (map set (map #(cons x %) (powerset (remove #{x} a-set))))))))
-    #{a-set}))
+    (list a-set)))
