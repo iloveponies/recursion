37f721895869a53072c3033c477178331af7bfe9
Lilja <triforce@windowslive.com>
2017-06-14 21:11:00 +0300

    exercises 14-26

--- src/recursion.clj
+++ src/recursion.clj
@@ -65,54 +65,103 @@
 
 (defn my-map [f seq-1 seq-2]
   (let [result (fn [] (f (first seq-1) (first seq-2)))]
-  (cond
-    (or (empty? seq-1) (empty? seq-2)) '()
-    :else (cons (result) (my-map f (rest seq-1) (rest seq-2))))))
+  (if (or (empty? seq-1) (empty? seq-2))
+    '()
+    (cons (result) (my-map f (rest seq-1) (rest seq-2))))))
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (= 0 n) 0
+    (= 1 n) 1
+    :else (+ (fib (- n 1))
+             (fib (- n 2)))))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (<= how-many-times 0)
+    '()
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (if (= 0 up-to)
+    '()
+    (cons (dec up-to) (my-range (dec up-to)))))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    ['()]
+    (cons a-seq (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    ['()]
+    (cons a-seq (inits (drop-last a-seq)))))
+
+(defn n-rotations [a-seq n]
+  (let [next-seq (fn [] (concat (rest a-seq) (vector (first a-seq))))]
+    (if (> n 0)
+      (cons a-seq (n-rotations (next-seq) (dec n))))))
 
 (defn rotations [a-seq]
-  [:-])
+  (if (= 0 (count a-seq))
+    '(())
+    (n-rotations a-seq (count a-seq))))
 
 (defn my-frequencies-helper [freqs a-seq]
-  [:-])
+  (if (empty? a-seq)
+    freqs
+    (let [inc-elem (fn [elem] (if (contains? freqs elem)
+                                (assoc freqs elem (inc (get freqs elem)))
+                                (assoc freqs elem 1)))]
+    (my-frequencies-helper (inc-elem (first a-seq)) (rest a-seq)))))
 
 (defn my-frequencies [a-seq]
-  [:-])
+  (my-frequencies-helper {} a-seq))
 
 (defn un-frequencies [a-map]
-  [:-])
+  (if (empty? a-map)
+    ()
+    (let [times (val (first a-map))
+          elem (key (first a-map))]
+      (concat (repeat times elem) (un-frequencies (rest a-map))))))
 
 (defn my-take [n coll]
-  [:-])
+  (if (or (= n 0)
+          (empty? coll))
+    '()
+    (cons (first coll) (my-take (dec n) (rest coll)))))
 
 (defn my-drop [n coll]
-  [:-])
+  (cond
+    (or (empty? coll)
+        (> n (count coll))) '()
+    (<= n 0) (cons (first coll) (my-drop (dec n) (rest coll)))
+    :else (my-drop (dec n) (rest coll))))
 
 (defn halve [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    '()
+    (let [middle (int (/ (count a-seq) 2))]
+      (vector (my-take middle a-seq) (my-drop middle a-seq)))))
 
 (defn seq-merge [a-seq b-seq]
-  [:-])
+  (cond
+    (and (empty? a-seq) (empty? b-seq)) ()
+    (empty? a-seq) b-seq
+    (empty? b-seq) a-seq
+    (< (first a-seq) (first b-seq)) (cons (first a-seq) (seq-merge (rest a-seq) b-seq))
+    :else (cons (first b-seq) (seq-merge a-seq (rest b-seq)))))
+
 
 (defn merge-sort [a-seq]
-  [:-])
+  (if (<= (count a-seq) 1)
+    a-seq
+    (let [half (halve a-seq)]
+      (seq-merge (merge-sort (first half)) (merge-sort (second half))))))
 
 (defn split-into-monotonics [a-seq]
   [:-])
