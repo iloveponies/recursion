696d6b6ab65409cab3ec75ef8b0bca1f19a89c6f
epicharri <harri.kahkonen3@gmail.com>
2017-07-07 18:30:17 +0300

    longest-sequence

--- src/recursion.clj
+++ src/recursion.clj
@@ -26,7 +26,10 @@
     seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (or (empty? a-seq) (singleton? a-seq))
+    (first a-seq)
+    (seq-max (first a-seq) 
+             (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
   [:-])
