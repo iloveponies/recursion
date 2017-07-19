65d21f5cd120dd89fe0b72f5e8994a14d396b9ad
Sopulius <aleksi.suoranta@gmail.com>
2017-07-19 14:07:25 +0300

    extras not done yet

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject recursion "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [iloveponies.tests/recursion "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
