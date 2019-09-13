9ad87fa2a056c33b473a952f29bc01febda561db
teo.mertanen <teo.mertanen@gofore.com>
2019-09-13 13:59:41 +0300

    Exercise 14 & 15

--- src/recursion.clj
+++ src/recursion.clj
@@ -94,10 +94,17 @@
   )
 
 (defn power [n k]
-  :-)
+  (if (zero? k)
+    1
+    (* n (power n (dec k)))
+    ))
 
 (defn fib [n]
-  :-)
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else (+ (fib (- n 1)) (fib (- n 2)))
+    ))
 
 (defn my-repeat [how-many-times what-to-repeat]
   [:-])
